94a516dd004bfe42f30107b2030eda37c5f9dbcc
tsalm0n <tsalm0n@twc.com>
2021-01-27 07:05:07 -0500

    routing

--- src/routes/v_InventoryMasterQuery_test.js
+++ src/routes/v_InventoryMasterQuery_test.js
@@ -1,3 +1,26 @@
 export async function post(req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
   console.log(`req.body==> ${req.body}`)
 }
+
+// export async function post(req, res, next) {
+//   console.log(`req.body==> ${req.body}`)
+// }
+
+// // routes/blog/[slug].json.js
+// import db from './_database.js'; // the underscore tells Sapper this isn't a route
+
+// export async function get(req, res, next) {
+// 	// the `slug` parameter is available because this file
+// 	// is called [slug].json.js
+// 	const { slug } = req.params;
+
+// 	const article = await db.get(slug);
+
+// 	if (article !== null) {
+// 		res.setHeader('Content-Type', 'application/json');
+// 		res.end(JSON.stringify(article));
+// 	} else {
+// 		next();
+// 	}
+// }
