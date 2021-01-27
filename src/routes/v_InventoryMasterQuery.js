cf4996ceee1167b48530af94c468b912269b3e5f
tsalm0n <tsalm0n@twc.com>
2021-01-27 05:53:53 -0500

    routing

--- src/routes/v_InventoryMasterQuery.js
+++ src/routes/v_InventoryMasterQuery.js
@@ -2,7 +2,7 @@ export function post(req, res, next) {
   const odbc = require('odbc')
   const DSN = process.env.ODBC_CONN_STRING
   console.log(`req==> ${req}`)
-  console.log(`JSON.stringify(req)==> ${JSON.stringify(req)}`)
+  console.log(`JSON.parse(req)==> ${JSON.parse(req)}`)
   console.log(`req.body==> ${req.body}`)
   const queryCatapultDBPostBody = req.body
   let catapultDbQuery = queryCatapultDBPostBody
