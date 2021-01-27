cfd11a163e9032e59f106fdc1a66cfe644c277e7
tsalm0n <tsalm0n@twc.com>
2021-01-27 07:39:12 -0500

    routing

--- src/routes/v_InventoryMasterQuery.js
+++ src/routes/v_InventoryMasterQuery.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 const odbc = require('odbc')
 const DSN = process.env.ODBC_CONN_STRING
 
