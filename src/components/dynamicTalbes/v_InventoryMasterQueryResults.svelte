837a37d88c99f31a09e1bd46ea384cb174af11d2
tsalm0n <tsalm0n@twc.com>
2021-01-31 23:12:08 -0500

    create dynamic table from tsql query results

--- src/components/dynamicTalbes/v_InventoryMasterQueryResults.svelte
+++ src/components/dynamicTalbes/v_InventoryMasterQueryResults.svelte
@@ -8,7 +8,7 @@ import tableData from "../dynamicTalbes/tableStores/tableData1.js";
     <table>
       <thead>
         <tr>
-          {#if tableData !== undefined || tableData !== null}
+          {#if tableData !== undefined && tableData !== null}
             {#each Object.keys(tableData[0][0]) as columnHeading}
               <th>{columnHeading}</th>
             {/each}
