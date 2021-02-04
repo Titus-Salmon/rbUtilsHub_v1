0d2da133ecd802bf8cbfd733e99e756f5ecc2c25
tsalm0n <tsalm0n@twc.com>
2021-02-03 22:59:57 -0500

    darkmode toggle

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -47,19 +47,19 @@ a {
   color: #0084f6;
   transition: background-color 0.3s;
 }
-:global(main) {
+/* :global(main) {
   background-color: #f2eee2;
   color: #0084f6;
   transition: background-color 0.3s;
-}
+} */
 :global(body.dark-mode) {
   background-color: #1d3040;
   color: #bfc2c7;
 }
-:global(main.dark-mode) {
+/* :global(main.dark-mode) {
   background-color: #1d3040;
   color: #bfc2c7;
-}
+} */
 /* t0d */
 </style>
 
