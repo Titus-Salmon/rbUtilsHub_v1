f53ed48e9eb29a928bb644bbab6f1d23977f28f2
tsalm0n <tsalm0n@twc.com>
2021-02-03 23:41:39 -0500

    darkmode toggle

--- src/components/dynamicTalbes/v_InventoryMasterQueryResults.svelte
+++ src/components/dynamicTalbes/v_InventoryMasterQueryResults.svelte
@@ -7,38 +7,29 @@ console.log(`table_data==> ${table_data}`);
 </script>
 
 <style>
-/* :global(body) {
-  background-color: #f2eee2;
-  color: #0084f6;
-  transition: background-color 0.3s;
-}
-:global(body.dark-mode) {
-  background-color: #1d3040;
-  color: #bfc2c7;
-} */
 </style>
 
-<body class="dark-mode">
+<!-- <body class="dark-mode"> -->
+<table>
   <table>
-    <table>
-      <thead>
+    <thead>
+      <tr>
+        {#if table_data !== undefined && table_data !== null && table_data !== ""}
+          {#each Object.keys(table_data[0]) as columnHeading}
+            <th>{columnHeading}</th>
+          {/each}
+        {/if}
+      </tr>
+    </thead>
+    <tbody>
+      {#each Object.values(table_data) as row}
         <tr>
-          {#if table_data !== undefined && table_data !== null && table_data !== ""}
-            {#each Object.keys(table_data[0]) as columnHeading}
-              <th>{columnHeading}</th>
-            {/each}
-          {/if}
+          {#each Object.values(row) as cell}
+            <td>{cell}</td>
+          {/each}
         </tr>
-      </thead>
-      <tbody>
-        {#each Object.values(table_data) as row}
-          <tr>
-            {#each Object.values(row) as cell}
-              <td>{cell}</td>
-            {/each}
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+      {/each}
+    </tbody>
   </table>
-</body>
+</table>
+<!-- </body> -->
