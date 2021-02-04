401548da0a673411925963b946327c58cacfc98d
tsalm0n <tsalm0n@twc.com>
2021-02-03 22:32:18 -0500

    darkmode toggle

--- src/routes/tsqlHub.svelte
+++ src/routes/tsqlHub.svelte
@@ -51,17 +51,6 @@ function vInvMasterQuery() {
 /* body {
   background-color: black;
 } */
-/* t0d */
-:global(body) {
-  background-color: #f2eee2;
-  color: #0084f6;
-  transition: background-color 0.3s;
-}
-:global(body.dark-mode) {
-  background-color: #1d3040;
-  color: #bfc2c7;
-}
-/* t0d */
 </style>
 
 <body>
