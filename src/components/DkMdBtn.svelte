7c363a8fc41c508603ec6982e4f63f54abb91f4d
tsalm0n <tsalm0n@twc.com>
2021-02-06 01:21:30 -0500

    css mods

--- src/components/DkMdBtn.svelte
+++ src/components/DkMdBtn.svelte
@@ -1,47 +1,39 @@
 <script>
 let clickCount = 0;
 function darkModeToggle() {
-  if (localStorage.getItem("colorScheme") !== null) {
-    localStorage.removeItem("colorScheme");
-  }
-  clickCount += 1;
-  clickCount % 2 !== 0 //clickCount "odd" condition
-    ? localStorage.setItem("colorScheme", "darkMode")
-    : localStorage.setItem("colorScheme", "lightMode");
-
-  // var allDocElements = window.document.getElementsByTagName("*");
-  // for (let i = 0; i < allDocElements.length; i++) {
-  //   allDocElements[i].classList.toggle("dark-mode");
+  //v//don't really need this for now, but putting color scheme designation on click into localStorage may be useful at some point
+  // if (localStorage.getItem("colorScheme") !== null) {
+  //   localStorage.removeItem("colorScheme");
   // }
+  // clickCount += 1;
+  // clickCount % 2 !== 0 //clickCount "odd" condition
+  //   ? localStorage.setItem("colorScheme", "darkMode")
+  //   : localStorage.setItem("colorScheme", "lightMode");
+  //^//don't really need this for now, but putting color scheme designation on click into localStorage may be useful at some point
 
   var allDocMainElems = window.document.getElementsByTagName("main");
   for (let i = 0; i < allDocMainElems.length; i++) {
-    // allDocTrElems[i].classList.remove("dark-mode");
-    allDocMainElems[i].classList.toggle("dark-mode-main");
+    allDocMainElems[i].classList.toggle("light-mode-main");
   }
 
   var allDocBodyElems = window.document.getElementsByTagName("body");
   for (let i = 0; i < allDocBodyElems.length; i++) {
-    // allDocTrElems[i].classList.remove("dark-mode");
-    allDocBodyElems[i].classList.toggle("dark-mode-body");
+    allDocBodyElems[i].classList.toggle("light-mode-body");
   }
 
   var allDocThElems = window.document.getElementsByTagName("th");
   for (let i = 0; i < allDocThElems.length; i++) {
-    // allDocTrElems[i].classList.remove("dark-mode");
-    allDocThElems[i].classList.toggle("dark-mode-th");
+    allDocThElems[i].classList.toggle("light-mode-th");
   }
 
   var allDocTrElems = window.document.getElementsByTagName("tr");
   for (let i = 0; i < allDocTrElems.length; i++) {
-    // allDocTrElems[i].classList.remove("dark-mode");
-    allDocTrElems[i].classList.toggle("dark-mode-tr");
+    allDocTrElems[i].classList.toggle("light-mode-tr");
   }
 
   var allDocButtonElems = window.document.getElementsByTagName("button");
   for (let i = 0; i < allDocButtonElems.length; i++) {
-    // allDocButtonElems[i].classList.remove("dark-mode");
-    allDocButtonElems[i].classList.toggle("dark-mode-button");
+    allDocButtonElems[i].classList.toggle("light-mode-button");
   }
 }
 </script>
