a7b4054203a10a5278224e1ded0b1f9e900a0b0f
tsalm0n <tsalm0n@twc.com>
2021-02-06 03:16:48 -0500

    nav  mods

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+import successkid from "images/successkid.jpg";
 </script>
 
 <style>
-	h1, figure, p {
+/* h1, figure, p {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -33,18 +33,21 @@
 		h1 {
 			font-size: 4em;
 		}
-	}
+	} */
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
+  <img alt="Success Kid" src="{successkid}" />
+  <figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+  <strong
+    >Try editing this file (src/routes/index.svelte) to test live reloading.</strong>
+</p>
