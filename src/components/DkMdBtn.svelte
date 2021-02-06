13c51ce956402a4f4aa5310234b7eeae3a7f4a86
tsalm0n <tsalm0n@twc.com>
2021-02-05 22:06:49 -0500

    css mods

--- src/components/DkMdBtn.svelte
+++ src/components/DkMdBtn.svelte
@@ -1,23 +1,19 @@
 <script>
 let clickCount = 0;
 function darkModeToggle() {
-  console.log(
-    `localStorage.getItem("colorScheme")_1==> ${localStorage.getItem(
-      "colorScheme"
-    )}`
-  );
   if (localStorage.getItem("colorScheme") !== null) {
     localStorage.removeItem("colorScheme");
   }
   clickCount += 1;
-  console.log(`clickCount % 2==> ${clickCount % 2}`);
-  console.log(`typeof (clickCount % 2)==> ${typeof (clickCount % 2)}`);
-  if (clickCount % 2 !== 0) {
-    //if clickCount is odd
-    localStorage.setItem("colorScheme", "darkMode");
-  } else {
-    localStorage.setItem("colorScheme", "lightMode");
-  }
+  clickCount % 2
+    ? localStorage.setItem("colorScheme", "darkMode")
+    : localStorage.setItem("colorScheme", "lightMode");
+  // if (clickCount % 2 !== 0) {
+  //   //if clickCount is odd
+  //   localStorage.setItem("colorScheme", "darkMode");
+  // } else {
+  //   localStorage.setItem("colorScheme", "lightMode");
+  // }
   console.log(
     `localStorage.getItem("colorScheme")_2==> ${localStorage.getItem(
       "colorScheme"
