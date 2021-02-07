b13acac0b8c8041d0d6f516ef56306b3c7138f83
tsalm0n <tsalm0n@twc.com>
2021-02-07 00:06:52 -0500

    colormode toggle troubleshoot

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,53 +1,58 @@
 <script>
-import successkid from "images/successkid.jpg";
+// import successkid from "images/successkid.jpg";
+import DkMdBtn from "../components/DkMdBtn.svelte"; //t0d
 </script>
 
 <style>
-/* h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+h1,
+figure,
+p {
+  text-align: center;
+  margin: 0 auto;
+}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+h1 {
+  font-size: 2.8em;
+  text-transform: uppercase;
+  font-weight: 700;
+  margin: 0 0 0.5em 0;
+}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+figure {
+  margin: 0 0 1em 0;
+}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+img {
+  width: 100%;
+  max-width: 400px;
+  margin: 0 0 1em 0;
+}
 
-	p {
-		margin: 1em auto;
-	}
+p {
+  margin: 1em auto;
+}
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	} */
+@media (min-width: 480px) {
+  h1 {
+    font-size: 4em;
+  }
+}
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>rbUtilsHub_v1</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<DkMdBtn>MODE</DkMdBtn>
 
-<figure>
+<h1>rbUtilsHub_v1</h1>
+
+<!-- <figure>
   <img alt="Success Kid" src="{successkid}" />
   <figcaption>Have fun with Sapper!</figcaption>
-</figure>
+</figure> -->
 
 <p>
   <strong
-    >Try editing this file (src/routes/index.svelte) to test live reloading.</strong>
+    >put something here for home page, possible login, or something else</strong>
 </p>
