16548eefec11e6194b661fc63ac17f041163d556
tsalm0n <tsalm0n@twc.com>
2021-02-07 15:58:23 -0500

    add save to CSV functionality

---
+++ src/routes/saveToCSV.js
@@ -0,0 +1,23 @@
+import tableData from "../dynamicTalbes/tableStores/tableData1.js"; //import table data from Svelte tableData1.js store
+let table_data;
+tableData.subscribe((tableData_t0d) => (table_data = tableData_t0d));
+console.log(`table_data[0] from saveToCSV==> ${table_data[0]}`);
+
+export async function post(req, res, next) {
+    const fs = require('fs')
+    const {
+        parseAsync
+    } = require('json2csv');
+    const fields = Object.keys(table_data[0]); //these are the table headers (column names)
+    const opts = {
+        fields
+    };
+
+    parseAsync(table_data, opts)
+        .then(csv => console.log(`csv from saveToCSV==> ${csv}`))
+        .then(fs.writeFile(process.cwd() + '/public/csv-to-insert/' + req.body.data + '.csv', csv, function (err) {
+            if (err) throw err;
+            console.log(`~~~~~>> ${req.body.data} saved<<~~~~~`)
+        }))
+        .catch(err => console.error(err));
+}
--- src/routes/tsqlHub.svelte
+++ src/routes/tsqlHub.svelte
@@ -5,6 +5,10 @@ import tableData from "../components/dynamicTalbes/tableStores/tableData1.js";
 import DkMdBtn from "../components/DkMdBtn.svelte"; //t0d
 
 let tsqlQueryText;
+let saveToCSVfilename;
+
+let queryResJSONArr = []; //this is to hold the entire JSON table data response outside the scope of the vInvMasterQuery
+//function, so the data can be accessible to the saveToCSV function, as well.
 
 function vInvMasterQuery() {
   fetch("v_InventoryMasterQuery", {
@@ -16,38 +20,46 @@ function vInvMasterQuery() {
     //mode: 'cors',
     body: JSON.stringify({
       data: tsqlQueryText.value,
-      // data: `SELECT INV_PK, INV_CPK, INV_ScanCode, ORD_SupplierStockNumber, INV_Name,
-      //   INV_Size, INV_ReceiptAlias, inv_default, convert(varchar(10),
-      //   POS_TimeStamp, 120), INV_DateCreated, INV_EMP_FK_CreatedBy,
-      //   ord_quantityinorderunit, oup_name, sto_number, dpt_name, dpt_number,
-      //   SIB_IdealMargin, ven_companyname, convert(varchar(10), inv_lastreceived,
-      //   120), convert(varchar(10), inv_lastsold, 120), inv_lastcost,
-      //   SIB_BasePrice, inv_onhand, inv_onorder, inv_intransit, inv_memo,
-      //   PI1_Description, PI2_Description, PI3_Description, PI4_Description,
-      //   INV_PowerField1, INV_PowerField2, INV_PowerField3, INV_PowerField4 FROM
-      //   catapult.ecrs.v_InventoryMaster WHERE trim(ven_companyname) IN
-      //   ('EDI-ALOE') AND trim(dpt_number) != '999999' ORDER BY PI1_Description,
-      //   PI2_Description`,
     }),
   })
-    .then((responseFromBackend_t0d) => responseFromBackend_t0d.json())
+    .then((queryRes) => queryRes.json())
     //^//not certain how this works, but the best I can describe for now is as follows:
-    //[1] the "responseFromBackend_t0d" argument represents the result of the previous fetch()
+    //[1] the "queryRes" argument represents the result of the previous fetch()
     //[2] this result must then be converted to JSON via the json() method on the frontend, even though it was already sent
     //from the backend as JSON
 
-    .then((responseFromBackend_t0d_convertedToJSON) => {
+    .then((queryResJSON) => {
       console.log(
-        `JSON.stringify(responseFromBackend_t0d_convertedToJSON[0])==> ${JSON.stringify(
-          responseFromBackend_t0d_convertedToJSON[0]
-        )}`
+        `JSON.stringify(queryResJSON[0])==> ${JSON.stringify(queryResJSON[0])}`
       );
-      tableData.set(responseFromBackend_t0d_convertedToJSON); //passing backend response to frontend "Store"
+      tableData.set(queryResJSON); //passing backend response to frontend "Store"
       //& we are overwriting the "Store" with set()
+      queryResJSONArr.push(queryResJSON); //push table query results to array outside of this function, so the results
+      //can be used by saveToCSV()
     });
-  //^//[3] then, the results from the 1st then() are passed as "responseFromBackend_t0d_convertedToJSON",
+  //^//[3] then, the results from the 1st then() are passed as "queryResJSON",
   //and at that point we can use this JSON object to do whatever with, such as stringify it, or
   //display it in a table on the frontend
+}
+
+function saveToCSV() {
+  fetch("saveToCSV", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data: saveToCSVfilename.value,
+    }),
+  })
+    .then((saveToCSVresult) => saveToCSVresult.json())
+    .then((saveToCSVresultJSON) => {
+      console.log(
+        `JSON.stringify(saveToCSVresultJSON[0])==> ${JSON.stringify(
+          saveToCSVresultJSON[0]
+        )}`
+      );
+    });
 }
 </script>
 
@@ -77,5 +89,13 @@ function vInvMasterQuery() {
 </textarea>
 
 <button on:click="{vInvMasterQuery}">vInvMasterQuery</button>
+
+<label for="saveToCSV">File Name</label>
+<input
+  type="text"
+  id="saveToCSV"
+  name="saveToCSV"
+  bind:this="{saveToCSVfilename}" />
+<button on:click="{saveToCSV}">saveToCSV</button>
 
 <V_InventoryMasterQueryResultsTable />
