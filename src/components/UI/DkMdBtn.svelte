6f4b5f400c104a108dc1fce1b974dfbd3febe8e3
tsalm0n <tsalm0n@twc.com>
2021-02-13 05:18:17 -0500

    color mode toggle

--- src/components/UI/DkMdBtn.svelte
+++ src/components/UI/DkMdBtn.svelte
@@ -20,60 +20,66 @@ function darkModeToggle() {
   );
   //^//don't really need this for now, but putting color scheme designation on click into localStorage may be useful at some point
 
-  var allDocNavElems = window.document.getElementsByTagName("nav");
-  for (let i = 0; i < allDocNavElems.length; i++) {
-    allDocNavElems[i].classList.toggle("light-mode-nav");
+  if (localStorage["colorScheme"] == "darkMode") {
+    document.documentElement.setItem("color-mode", "dark");
+  } else {
+    document.documentElement.setItem("color-mode", "dark");
   }
 
-  var allDocMainElems = window.document.getElementsByTagName("main");
-  for (let i = 0; i < allDocMainElems.length; i++) {
-    allDocMainElems[i].classList.toggle("light-mode-main");
-  }
+  // var allDocNavElems = window.document.getElementsByTagName("nav");
+  // for (let i = 0; i < allDocNavElems.length; i++) {
+  //   allDocNavElems[i].classList.toggle("light-mode-nav");
+  // }
 
-  var allDocBodyElems = window.document.getElementsByTagName("body");
-  for (let i = 0; i < allDocBodyElems.length; i++) {
-    allDocBodyElems[i].classList.toggle("light-mode-body");
-  }
+  // var allDocMainElems = window.document.getElementsByTagName("main");
+  // for (let i = 0; i < allDocMainElems.length; i++) {
+  //   allDocMainElems[i].classList.toggle("light-mode-main");
+  // }
 
-  var allDocThElems = window.document.getElementsByTagName("th");
-  for (let i = 0; i < allDocThElems.length; i++) {
-    allDocThElems[i].classList.toggle("light-mode-th");
-  }
+  // var allDocBodyElems = window.document.getElementsByTagName("body");
+  // for (let i = 0; i < allDocBodyElems.length; i++) {
+  //   allDocBodyElems[i].classList.toggle("light-mode-body");
+  // }
 
-  var allDocTrElems = window.document.getElementsByTagName("tr");
-  for (let i = 0; i < allDocTrElems.length; i++) {
-    allDocTrElems[i].classList.toggle("light-mode-tr");
-  }
+  // var allDocThElems = window.document.getElementsByTagName("th");
+  // for (let i = 0; i < allDocThElems.length; i++) {
+  //   allDocThElems[i].classList.toggle("light-mode-th");
+  // }
 
-  var allDocButtonElems = window.document.getElementsByTagName("button");
-  for (let i = 0; i < allDocButtonElems.length; i++) {
-    allDocButtonElems[i].classList.toggle("light-mode-button");
-  }
+  // var allDocTrElems = window.document.getElementsByTagName("tr");
+  // for (let i = 0; i < allDocTrElems.length; i++) {
+  //   allDocTrElems[i].classList.toggle("light-mode-tr");
+  // }
 
-  var allDocTextAreaElems = window.document.getElementsByTagName("textarea");
-  for (let i = 0; i < allDocTextAreaElems.length; i++) {
-    allDocTextAreaElems[i].classList.toggle("light-mode-textarea");
-  }
+  // var allDocButtonElems = window.document.getElementsByTagName("button");
+  // for (let i = 0; i < allDocButtonElems.length; i++) {
+  //   allDocButtonElems[i].classList.toggle("light-mode-button");
+  // }
 
-  var allDocLabelElems = window.document.getElementsByTagName("label");
-  for (let i = 0; i < allDocLabelElems.length; i++) {
-    allDocLabelElems[i].classList.toggle("light-mode-label");
-  }
+  // var allDocTextAreaElems = window.document.getElementsByTagName("textarea");
+  // for (let i = 0; i < allDocTextAreaElems.length; i++) {
+  //   allDocTextAreaElems[i].classList.toggle("light-mode-textarea");
+  // }
 
-  var allDocInputElems = window.document.getElementsByTagName("input");
-  for (let i = 0; i < allDocInputElems.length; i++) {
-    allDocInputElems[i].classList.toggle("light-mode-input");
-  }
+  // var allDocLabelElems = window.document.getElementsByTagName("label");
+  // for (let i = 0; i < allDocLabelElems.length; i++) {
+  //   allDocLabelElems[i].classList.toggle("light-mode-label");
+  // }
 
-  var allDocPElems = window.document.getElementsByTagName("p");
-  for (let i = 0; i < allDocPElems.length; i++) {
-    allDocPElems[i].classList.toggle("light-mode-p");
-  }
+  // var allDocInputElems = window.document.getElementsByTagName("input");
+  // for (let i = 0; i < allDocInputElems.length; i++) {
+  //   allDocInputElems[i].classList.toggle("light-mode-input");
+  // }
 
-  var allDocAElems = window.document.getElementsByTagName("a");
-  for (let i = 0; i < allDocAElems.length; i++) {
-    allDocAElems[i].classList.toggle("light-mode-a");
-  }
+  // var allDocPElems = window.document.getElementsByTagName("p");
+  // for (let i = 0; i < allDocPElems.length; i++) {
+  //   allDocPElems[i].classList.toggle("light-mode-p");
+  // }
+
+  // var allDocAElems = window.document.getElementsByTagName("a");
+  // for (let i = 0; i < allDocAElems.length; i++) {
+  //   allDocAElems[i].classList.toggle("light-mode-a");
+  // }
 }
 </script>
 
