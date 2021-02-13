0e9f1d79298ef22d04c13e42164bad54c6854b76
tsalm0n <tsalm0n@twc.com>
2021-02-13 03:41:47 -0500

    restructuring

--- src/components/UI/DkMdBtn.svelte
+++ src/components/UI/DkMdBtn.svelte
@@ -7,67 +7,69 @@ function darkModeToggle() {
   }
   clickCount += 1;
   clickCount % 2 !== 0 //clickCount "odd" condition
-    ? localStorage.setItem("colorScheme", "darkMode")
-    : localStorage.setItem("colorScheme", "lightMode");
+    ? localStorage.setItem("colorScheme", "lightMode")
+    : localStorage.setItem("colorScheme", "darkMode");
 
   console.log(
     `JSON.stringify(localStorage)==> ${JSON.stringify(localStorage)}`
   );
   //^//don't really need this for now, but putting color scheme designation on click into localStorage may be useful at some point
 
-  var allDocNavElems = window.document.getElementsByTagName("nav");
-  for (let i = 0; i < allDocNavElems.length; i++) {
-    allDocNavElems[i].classList.toggle("light-mode-nav");
-  }
+  if (localStorage["colorScheme"] !== "darkMode") {
+    var allDocNavElems = window.document.getElementsByTagName("nav");
+    for (let i = 0; i < allDocNavElems.length; i++) {
+      allDocNavElems[i].classList.toggle("light-mode-nav");
+    }
 
-  var allDocMainElems = window.document.getElementsByTagName("main");
-  for (let i = 0; i < allDocMainElems.length; i++) {
-    allDocMainElems[i].classList.toggle("light-mode-main");
-  }
+    var allDocMainElems = window.document.getElementsByTagName("main");
+    for (let i = 0; i < allDocMainElems.length; i++) {
+      allDocMainElems[i].classList.toggle("light-mode-main");
+    }
 
-  var allDocBodyElems = window.document.getElementsByTagName("body");
-  for (let i = 0; i < allDocBodyElems.length; i++) {
-    allDocBodyElems[i].classList.toggle("light-mode-body");
-  }
+    var allDocBodyElems = window.document.getElementsByTagName("body");
+    for (let i = 0; i < allDocBodyElems.length; i++) {
+      allDocBodyElems[i].classList.toggle("light-mode-body");
+    }
 
-  var allDocThElems = window.document.getElementsByTagName("th");
-  for (let i = 0; i < allDocThElems.length; i++) {
-    allDocThElems[i].classList.toggle("light-mode-th");
-  }
+    var allDocThElems = window.document.getElementsByTagName("th");
+    for (let i = 0; i < allDocThElems.length; i++) {
+      allDocThElems[i].classList.toggle("light-mode-th");
+    }
 
-  var allDocTrElems = window.document.getElementsByTagName("tr");
-  for (let i = 0; i < allDocTrElems.length; i++) {
-    allDocTrElems[i].classList.toggle("light-mode-tr");
-  }
+    var allDocTrElems = window.document.getElementsByTagName("tr");
+    for (let i = 0; i < allDocTrElems.length; i++) {
+      allDocTrElems[i].classList.toggle("light-mode-tr");
+    }
 
-  var allDocButtonElems = window.document.getElementsByTagName("button");
-  for (let i = 0; i < allDocButtonElems.length; i++) {
-    allDocButtonElems[i].classList.toggle("light-mode-button");
-  }
+    var allDocButtonElems = window.document.getElementsByTagName("button");
+    for (let i = 0; i < allDocButtonElems.length; i++) {
+      allDocButtonElems[i].classList.toggle("light-mode-button");
+    }
 
-  var allDocTextAreaElems = window.document.getElementsByTagName("textarea");
-  for (let i = 0; i < allDocTextAreaElems.length; i++) {
-    allDocTextAreaElems[i].classList.toggle("light-mode-textarea");
-  }
+    var allDocTextAreaElems = window.document.getElementsByTagName("textarea");
+    for (let i = 0; i < allDocTextAreaElems.length; i++) {
+      allDocTextAreaElems[i].classList.toggle("light-mode-textarea");
+    }
 
-  var allDocLabelElems = window.document.getElementsByTagName("label");
-  for (let i = 0; i < allDocLabelElems.length; i++) {
-    allDocLabelElems[i].classList.toggle("light-mode-label");
-  }
+    var allDocLabelElems = window.document.getElementsByTagName("label");
+    for (let i = 0; i < allDocLabelElems.length; i++) {
+      allDocLabelElems[i].classList.toggle("light-mode-label");
+    }
 
-  var allDocInputElems = window.document.getElementsByTagName("input");
-  for (let i = 0; i < allDocInputElems.length; i++) {
-    allDocInputElems[i].classList.toggle("light-mode-input");
-  }
+    var allDocInputElems = window.document.getElementsByTagName("input");
+    for (let i = 0; i < allDocInputElems.length; i++) {
+      allDocInputElems[i].classList.toggle("light-mode-input");
+    }
 
-  var allDocPElems = window.document.getElementsByTagName("p");
-  for (let i = 0; i < allDocPElems.length; i++) {
-    allDocPElems[i].classList.toggle("light-mode-p");
-  }
+    var allDocPElems = window.document.getElementsByTagName("p");
+    for (let i = 0; i < allDocPElems.length; i++) {
+      allDocPElems[i].classList.toggle("light-mode-p");
+    }
 
-  var allDocAElems = window.document.getElementsByTagName("a");
-  for (let i = 0; i < allDocAElems.length; i++) {
-    allDocAElems[i].classList.toggle("light-mode-a");
+    var allDocAElems = window.document.getElementsByTagName("a");
+    for (let i = 0; i < allDocAElems.length; i++) {
+      allDocAElems[i].classList.toggle("light-mode-a");
+    }
   }
 }
 </script>
