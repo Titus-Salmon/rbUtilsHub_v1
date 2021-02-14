dd825e3b9bd6da0f288035f5b699d080ae43e4dc
tsalm0n <tsalm0n@twc.com>
2021-02-13 23:24:01 -0500

    css mods

--- src/routes/pages/pg_T-SQL/tsqlHub.svelte
+++ src/routes/pages/pg_T-SQL/tsqlHub.svelte
@@ -8,6 +8,18 @@ import DkMdBtn from "../../../components/UI/DkMdBtn.svelte";
 let tsqlQueryText;
 let saveToCSVfilename;
 let saveToCSVresponse;
+let queryText = `
+SELECT 
+INV_PK, INV_CPK, INV_ScanCode, ORD_SupplierStockNumber, INV_Name, INV_Size, INV_ReceiptAlias, inv_default, 
+convert(varchar(10), POS_TimeStamp, 120), INV_DateCreated, INV_EMP_FK_CreatedBy, ord_quantityinorderunit, oup_name, sto_number, 
+dpt_name, dpt_number, SIB_IdealMargin, ven_companyname, convert(varchar(10), inv_lastreceived, 120), convert(varchar(10), inv_lastsold, 120), 
+inv_lastcost, SIB_BasePrice, inv_onhand, inv_onorder, inv_intransit, inv_memo, PI1_Description, PI2_Description, PI3_Description, 
+PI4_Description, INV_PowerField1, INV_PowerField2, INV_PowerField3, INV_PowerField4 
+FROM
+catapult.ecrs.v_InventoryMaster 
+WHERE trim(ven_companyname) IN ('EDI-ALOE')
+AND trim(dpt_number) != '999999' ORDER BY PI1_Description, PI2_Description
+`;
 
 function vInvMasterQuery() {
   fetch("server_routes/rt_T-SQL/v_InventoryMasterQuery", {
@@ -74,17 +86,7 @@ function saveToCSV() {
     rows="1"
     wrap="soft"
     bind:this="{tsqlQueryText}">
-    SELECT INV_PK, INV_CPK, INV_ScanCode, ORD_SupplierStockNumber, INV_Name,
-    INV_Size, INV_ReceiptAlias, inv_default, convert(varchar(10), POS_TimeStamp,
-    120), INV_DateCreated, INV_EMP_FK_CreatedBy, ord_quantityinorderunit,
-    oup_name, sto_number, dpt_name, dpt_number, SIB_IdealMargin,
-    ven_companyname, convert(varchar(10), inv_lastreceived, 120),
-    convert(varchar(10), inv_lastsold, 120), inv_lastcost, SIB_BasePrice,
-    inv_onhand, inv_onorder, inv_intransit, inv_memo, PI1_Description,
-    PI2_Description, PI3_Description, PI4_Description, INV_PowerField1,
-    INV_PowerField2, INV_PowerField3, INV_PowerField4 FROM
-    catapult.ecrs.v_InventoryMaster WHERE trim(ven_companyname) IN ('EDI-ALOE')
-    AND trim(dpt_number) != '999999' ORDER BY PI1_Description, PI2_Description
+    {queryText}
   </textarea>
 </div>
 
