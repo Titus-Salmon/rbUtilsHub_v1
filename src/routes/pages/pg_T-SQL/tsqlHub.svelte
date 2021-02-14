94ba50af7dc6f4449294bd43b5bca20bfee7b2b3
tsalm0n <tsalm0n@twc.com>
2021-02-13 23:09:29 -0500

    css mods

--- src/routes/pages/pg_T-SQL/tsqlHub.svelte
+++ src/routes/pages/pg_T-SQL/tsqlHub.svelte
@@ -66,26 +66,27 @@ function saveToCSV() {
 
 <DkMdBtn>MODE</DkMdBtn>
 
-<textarea
-  class="query1"
-  id="tsqlQueryText"
-  name="tblQryPost"
-  cols="30"
-  rows="1"
-  wrap="soft"
-  bind:this="{tsqlQueryText}">
-  SELECT INV_PK, INV_CPK, INV_ScanCode, ORD_SupplierStockNumber, INV_Name,
-  INV_Size, INV_ReceiptAlias, inv_default, convert(varchar(10), POS_TimeStamp,
-  120), INV_DateCreated, INV_EMP_FK_CreatedBy, ord_quantityinorderunit,
-  oup_name, sto_number, dpt_name, dpt_number, SIB_IdealMargin, ven_companyname,
-  convert(varchar(10), inv_lastreceived, 120), convert(varchar(10),
-  inv_lastsold, 120), inv_lastcost, SIB_BasePrice, inv_onhand, inv_onorder,
-  inv_intransit, inv_memo, PI1_Description, PI2_Description, PI3_Description,
-  PI4_Description, INV_PowerField1, INV_PowerField2, INV_PowerField3,
-  INV_PowerField4 FROM catapult.ecrs.v_InventoryMaster WHERE
-  trim(ven_companyname) IN ('EDI-ALOE') AND trim(dpt_number) != '999999' ORDER
-  BY PI1_Description, PI2_Description
-</textarea>
+<div text-align="center">
+  <textarea
+    id="tsqlQueryText"
+    name="tblQryPost"
+    cols="30"
+    rows="1"
+    wrap="soft"
+    bind:this="{tsqlQueryText}">
+    SELECT INV_PK, INV_CPK, INV_ScanCode, ORD_SupplierStockNumber, INV_Name,
+    INV_Size, INV_ReceiptAlias, inv_default, convert(varchar(10), POS_TimeStamp,
+    120), INV_DateCreated, INV_EMP_FK_CreatedBy, ord_quantityinorderunit,
+    oup_name, sto_number, dpt_name, dpt_number, SIB_IdealMargin,
+    ven_companyname, convert(varchar(10), inv_lastreceived, 120),
+    convert(varchar(10), inv_lastsold, 120), inv_lastcost, SIB_BasePrice,
+    inv_onhand, inv_onorder, inv_intransit, inv_memo, PI1_Description,
+    PI2_Description, PI3_Description, PI4_Description, INV_PowerField1,
+    INV_PowerField2, INV_PowerField3, INV_PowerField4 FROM
+    catapult.ecrs.v_InventoryMaster WHERE trim(ven_companyname) IN ('EDI-ALOE')
+    AND trim(dpt_number) != '999999' ORDER BY PI1_Description, PI2_Description
+  </textarea>
+</div>
 
 <button on:click="{vInvMasterQuery}">vInvMasterQuery</button>
 
