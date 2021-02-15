355f833f2394e2e976a8822ad78b4d9aef54a349
tsalm0n <tsalm0n@twc.com>
2021-02-15 04:55:04 -0500

    refactoring experiment

--- src/routes/pages/pg_T-SQL/tsqlHub.svelte
+++ src/routes/pages/pg_T-SQL/tsqlHub.svelte
@@ -130,7 +130,7 @@ function saveToXLSX() {
     class="query"
     id="tsqlQueryText"
     name="tblQryPost"
-    cols="175"
+    cols="166"
     rows="15"
     wrap="soft"
     bind:this="{tsqlQueryText}">
