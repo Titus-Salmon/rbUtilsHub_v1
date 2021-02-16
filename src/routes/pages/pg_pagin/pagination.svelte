6640df2a1a2f1407a96336647201a22f5a543ddd
tsalm0n <tsalm0n@twc.com>
2021-02-16 15:53:53 -0500

    pagination

--- src/routes/pages/pg_pagin/pagination.svelte
+++ src/routes/pages/pg_pagin/pagination.svelte
@@ -5,48 +5,52 @@ import tableData from "../../../stores/dynamicTables/tableData1.js";
 // let pagin_data;
 // paginData.subscribe((paginData_t0d) => (pagin_data = paginData_t0d));
 
-let pageToDisplay = 0;
-let forward;
-let reverse;
+let pageToDisplay;
+// let forward;
+// let reverse;
 
 function paginate(direction) {
-  console.log(`$paginData==> ${$paginData}`);
-  console.log(`Object.keys($paginData)1==> ${Object.keys($paginData)}`);
-  console.log(`Object.values($paginData)1==> ${Object.values($paginData)}`);
+  if (typeof $paginData.totalPages === "number") {
+    console.log(`$paginData==> ${$paginData}`);
+    console.log(`Object.keys($paginData)1==> ${Object.keys($paginData)}`);
+    console.log(`Object.values($paginData)1==> ${Object.values($paginData)}`);
 
-  if (direction === "forward") {
-    pageToDisplay += 1;
+    if (direction === "forward") {
+      pageToDisplay = $paginData.currentPage + 1;
+    }
+    if (direction === "reverse") {
+      pageToDisplay = $paginData.currentPage - 1;
+    }
+
+    fetch(`server_routes/pagination/rt_pagination?page=${pageToDisplay}`, {
+      method: "GET",
+    })
+      .then((queryRes) => queryRes.json())
+      .then((queryResJSON) => {
+        console.log(`queryResJSON from pagination.svelte==> ${queryResJSON}`);
+        console.log(
+          `JSON.stringify(queryResJSON) from pagination.svelte==> ${JSON.stringify(
+            queryResJSON
+          )}`
+        );
+        tableData.set(queryResJSON.catapultResArr_pagin); //passing backend response to frontend "Store"
+        //& we are overwriting the "Store" with set()
+        paginData.set({
+          currentPage: queryResJSON.currentPage,
+          nextPage: queryResJSON.nextPage,
+          prevPage: queryResJSON.prevPage,
+        }); //passing backend response to frontend "Store"
+        //& we are overwriting the "Store" with set()
+
+        console.log(`Object.keys($paginData)2==> ${Object.keys($paginData)}`);
+        console.log(
+          `Object.values($paginData)2==> ${Object.values($paginData)}`
+        );
+      });
+    //^//[3] then, the results from the 1st then() are passed as "queryResJSON",
+    //and at that point we can use this JSON object to do whatever with, such as stringify it, or
+    //display it in a table on the frontend
   }
-  if (direction === "reverse") {
-    pageToDisplay -= 1;
-  }
-
-  fetch(`server_routes/pagination/rt_pagination?page=${pageToDisplay}`, {
-    method: "GET",
-  })
-    .then((queryRes) => queryRes.json())
-    .then((queryResJSON) => {
-      console.log(`queryResJSON from pagination.svelte==> ${queryResJSON}`);
-      console.log(
-        `JSON.stringify(queryResJSON) from pagination.svelte==> ${JSON.stringify(
-          queryResJSON
-        )}`
-      );
-      tableData.set(queryResJSON.catapultResArr_pagin); //passing backend response to frontend "Store"
-      //& we are overwriting the "Store" with set()
-      paginData.set({
-        currentPage: queryResJSON.currentPage,
-        nextPage: queryResJSON.nextPage,
-        prevPage: queryResJSON.prevPage,
-      }); //passing backend response to frontend "Store"
-      //& we are overwriting the "Store" with set()
-
-      console.log(`Object.keys($paginData)2==> ${Object.keys($paginData)}`);
-      console.log(`Object.values($paginData)2==> ${Object.values($paginData)}`);
-    });
-  //^//[3] then, the results from the 1st then() are passed as "queryResJSON",
-  //and at that point we can use this JSON object to do whatever with, such as stringify it, or
-  //display it in a table on the frontend
 }
 </script>
 
