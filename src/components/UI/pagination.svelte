a6c7edd4d67460454aefdd1cbd6e18f489831b76
tsalm0n <tsalm0n@twc.com>
2021-02-15 22:09:44 -0500

    pagination

--- src/components/UI/pagination.svelte
+++ src/components/UI/pagination.svelte
@@ -1,15 +1,16 @@
 <script>
 import paginData from "../../stores/pagination/pagination1.js";
-let pagin_data;
-paginData.subscribe((paginData_t0d) => (pagin_data = paginData_t0d));
+// let pagin_data;
+// paginData.subscribe((paginData_t0d) => (pagin_data = paginData_t0d));
 </script>
 
 <style>
 </style>
 
 <body>
-  {#if pagin_data[0] !== undefined}
-    <p>Object.keys(pagin_data)==> {Object.keys(pagin_data)}</p>
-    <p>pagin_data[0]==> {pagin_data[0]}</p>
-  {/if}
+  <!-- {#if pagin_data[0] !== undefined} -->
+  <!-- <p>Object.keys(pagin_data)==> {Object.keys(pagin_data)}</p>
+    <p>pagin_data[0]==> {pagin_data[0]}</p> -->
+  <p>$paginData==> {$paginData}</p>
+  <!-- {/if} -->
 </body>
