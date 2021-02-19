6c7007624e12e13f37887e0ec24600a0a91a598b
tsalm0n <tsalm0n@twc.com>
2021-02-19 05:47:40 -0500

    saveToXLSX abstraction

--- src/routes/server_routes/rt_T-SQL/saveToXLSX.js
+++ src/routes/server_routes/rt_T-SQL/saveToXLSX.js
@@ -9,13 +9,15 @@ export async function post(req, res, next) {
   catapultResArrCacheValue = catapultResArrCache.take('catapultResArrCache_key') // this also deletes the key
   console.log(`JSON.stringify(catapultResArrCacheValue[0])==> ${JSON.stringify(catapultResArrCacheValue[0])}`)
 
-  //NOTE++++++++>>> srcRsXLS_tsql is the original array that holds the collection of SearchResults objects {columnName: cellValue}
-  //HOWEVER, since the inherent order (from showSearchResults()) of these key:value pairs is not the order we want to display them
-  //in the excel file, and also since there are additional key:value pairs from srcRsXLS_tsql that we DON'T want to display (i.e.
-  //invPK, invCPK), we selectively reorder and/or remove the key:value pairs from srcRsXLS_tsql to form the srcRsXLS_selectiveReordering array
-  //(WITHOUT modifying the original srcRsXLS_tsql array).
+  ///̶/̶N̶O̶T̶E̶+̶+̶+̶+̶+̶+̶+̶+̶>̶>̶>̶ s̶r̶c̶R̶s̶X̶L̶S̶_̶t̶s̶q̶l̶ i̶s̶ t̶h̶e̶ o̶r̶i̶g̶i̶n̶a̶l̶ a̶r̶r̶a̶y̶ t̶h̶a̶t̶ h̶o̶l̶d̶s̶ t̶h̶e̶ c̶o̶l̶l̶e̶c̶t̶i̶o̶n̶ o̶f̶ S̶e̶a̶r̶c̶h̶R̶e̶s̶u̶l̶t̶s̶ o̶b̶j̶e̶c̶t̶s̶ {̶c̶o̶l̶u̶m̶n̶N̶a̶m̶e̶:̶ c̶e̶l̶l̶V̶a̶l̶u̶e̶}̶
+  ///̶/̶H̶O̶W̶E̶V̶E̶R̶,̶ s̶i̶n̶c̶e̶ t̶h̶e̶ i̶n̶h̶e̶r̶e̶n̶t̶ o̶r̶d̶e̶r̶ (̶f̶r̶o̶m̶ s̶h̶o̶w̶S̶e̶a̶r̶c̶h̶R̶e̶s̶u̶l̶t̶s̶(̶)̶)̶ o̶f̶ t̶h̶e̶s̶e̶ k̶e̶y̶:̶v̶a̶l̶u̶e̶ p̶a̶i̶r̶s̶ i̶s̶ n̶o̶t̶ t̶h̶e̶ o̶r̶d̶e̶r̶ w̶e̶ w̶a̶n̶t̶ t̶o̶ d̶i̶s̶p̶l̶a̶y̶ t̶h̶e̶m̶
+  ///̶/̶i̶n̶ t̶h̶e̶ e̶x̶c̶e̶l̶ f̶i̶l̶e̶,̶ a̶n̶d̶ a̶l̶s̶o̶ s̶i̶n̶c̶e̶ t̶h̶e̶r̶e̶ a̶r̶e̶ a̶d̶d̶i̶t̶i̶o̶n̶a̶l̶ k̶e̶y̶:̶v̶a̶l̶u̶e̶ p̶a̶i̶r̶s̶ f̶r̶o̶m̶ s̶r̶c̶R̶s̶X̶L̶S̶_̶t̶s̶q̶l̶ t̶h̶a̶t̶ w̶e̶ D̶O̶N̶'̶T̶ w̶a̶n̶t̶ t̶o̶ d̶i̶s̶p̶l̶a̶y̶ (̶i̶.̶e̶.̶
+  ///̶/̶i̶n̶v̶P̶K̶,̶ i̶n̶v̶C̶P̶K̶)̶,̶ w̶e̶ s̶e̶l̶e̶c̶t̶i̶v̶e̶l̶y̶ r̶e̶o̶r̶d̶e̶r̶ a̶n̶d̶/̶o̶r̶ r̶e̶m̶o̶v̶e̶ t̶h̶e̶ k̶e̶y̶:̶v̶a̶l̶u̶e̶ p̶a̶i̶r̶s̶ f̶r̶o̶m̶ s̶r̶c̶R̶s̶X̶L̶S̶_̶t̶s̶q̶l̶ t̶o̶ f̶o̶r̶m̶ t̶h̶e̶ s̶r̶c̶R̶s̶X̶L̶S̶_̶s̶e̶l̶e̶c̶t̶i̶v̶e̶R̶e̶o̶r̶d̶e̶r̶i̶n̶g̶ a̶r̶r̶a̶y̶
+  ///̶/̶(̶W̶I̶T̶H̶O̶U̶T̶ m̶o̶d̶i̶f̶y̶i̶n̶g̶ t̶h̶e̶ o̶r̶i̶g̶i̶n̶a̶l̶ s̶r̶c̶R̶s̶X̶L̶S̶_̶t̶s̶q̶l̶ a̶r̶r̶a̶y̶)̶.̶
 
   var srcRsXLS_selectiveReordering = []
+
+  //Need to provide abstraction to allow for any query's column combination/selection////////////////////////////////////
 
   for (let a = 0; a < catapultResArrCacheValue.length; a++) {
     let reorderedResObj = {}
