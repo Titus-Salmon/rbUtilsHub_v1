ecf6943aff37df30d90cf9895c1d0be8d196136d
tsalm0n <tsalm0n@twc.com>
2021-02-23 02:51:58 -0500

    modularize queries

--- src/components/MySQL/querySelectDropdown.svelte
+++ src/components/MySQL/querySelectDropdown.svelte
@@ -4,27 +4,31 @@ import ShowTablesQueryModal from "./showTablesQueryModal.svelte";
 
 // const querySelect = window.document.getElementById("querySelect");
 // const selectedQuery = querySelect.options[querySelect.selectedIndex].value;
-let querySelect;
+// let querySelect;
+let selected;
 </script>
 
 <style></style>
 
 <div>
-  <select name="querySelect" id="querySelect">
-    <option value="nej" bind:this="{querySelect}">nej</option>
-    <option value="showTables" bind:this="{querySelect}">showTables</option>
-    <option value="createNhcrt" bind:this="{querySelect}">createNhcrt</option>
-    <option value="createNej" bind:this="{querySelect}">createNej</option>
-    <option value="populateTable" bind:this="{querySelect}"
-      >populateTable</option>
-    <option value="deleteTables" bind:this="{querySelect}">deleteTables</option>
+  <select name="querySelect" id="querySelect" bind:value="{selected}">
+    <option value="nej">nej</option>
+    <option value="showTables">showTables</option>
+    <option value="createNhcrt">createNhcrt</option>
+    <option value="createNej">createNej</option>
+    <option value="populateTable">populateTable</option>
+    <option value="deleteTables">deleteTables</option>
   </select>
+
+  <!-- <select bind:value="{selected}" on:change="{() => (concs = selected)}">
+    ...
+  </select> -->
 </div>
 
-{#if querySelect.value === "nej"}
+{#if selected === "nej"}
   <NejQueryModal />
 {/if}
 
-{#if querySelect.value === "showTables"}
+{#if selected === "showTables"}
   <ShowTablesQueryModal />
 {/if}
