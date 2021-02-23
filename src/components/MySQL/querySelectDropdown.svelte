e3a4ead3ab554578b2547a33097772ca033a6fc5
tsalm0n <tsalm0n@twc.com>
2021-02-23 02:42:37 -0500

    modularize queries

--- src/components/MySQL/querySelectDropdown.svelte
+++ src/components/MySQL/querySelectDropdown.svelte
@@ -10,20 +10,21 @@ let querySelect;
 <style></style>
 
 <div>
-  <select name="querySelect" id="querySelect" bind:this="{querySelect}">
-    <option value="nej">nej</option>
-    <option value="showTables">showTables</option>
-    <option value="createNhcrt">createNhcrt</option>
-    <option value="createNej">createNej</option>
-    <option value="populateTable">populateTable</option>
-    <option value="deleteTables">deleteTables</option>
+  <select name="querySelect" id="querySelect">
+    <option value="nej" bind:this="{querySelect}">nej</option>
+    <option value="showTables" bind:this="{querySelect}">showTables</option>
+    <option value="createNhcrt" bind:this="{querySelect}">createNhcrt</option>
+    <option value="createNej" bind:this="{querySelect}">createNej</option>
+    <option value="populateTable" bind:this="{querySelect}"
+      >populateTable</option>
+    <option value="deleteTables" bind:this="{querySelect}">deleteTables</option>
   </select>
 </div>
 
-{#if querySelect.options[querySelect.selectedIndex].value === "nej"}
+{#if querySelect.value === "nej"}
   <NejQueryModal />
 {/if}
 
-{#if querySelect.options[querySelect.selectedIndex].value === "showTables"}
+{#if querySelect.value === "showTables"}
   <ShowTablesQueryModal />
 {/if}
