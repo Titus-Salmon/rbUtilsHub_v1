7fe542771b867a7afc18ea8110dfa82872fefb73
tsalm0n <tsalm0n@twc.com>
2021-02-24 05:00:44 -0500

    create & populate  table modal restructuring

--- src/components/MySQL/populateTableModal.svelte
+++ src/components/MySQL/populateTableModal.svelte
@@ -4,94 +4,64 @@ let popTableFile;
 let popTableTableName;
 let popTableResponse;
 
-// const formData = new FormData();
-// formData.append("popTableTableName", popTableTableName);
-// formData.append("popTableFile", popTableFile);
+// let files;
+// let statusCode = "";
 
-// console.log(`formData==> ${formData}`);
-// console.log(`JSON.stringify(formData)==> ${JSON.stringify(formData)}`);
+// async function handleSubmit() {
+//   if (files.length > 0) {
+//     const formData = new FormData();
+//     formData.append("file", files[0]);
+//     const response = await fetch("/gateway/media_objects", {
+//       method: "POST",
+//       body: formData,
+//     });
+//     statusCode = response.status;
+//   }
+// }
 
-function upload() {
-  const formData = new FormData();
-  formData.append("damName", value);
-  formData.append("dataFile", files[0]);
-  const upload = fetch("http://localhost:8080/file", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("Success:", result);
+async function popTable() {
+  if (popTableFile.length > 0) {
+    const formData = new FormData();
+    formData.append("popTableTableName", popTableTableName.value);
+    formData.append("popTableFile", popTableFile[0]);
+
+    await fetch("server_routes/rt_MySQL/popTable", {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      // body: JSON.stringify({
+      //   popTableFile: popTableFile.value,
+      //   popTableTableName: popTableTableName.value,
+      // }),
+      body: formData,
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+      .then(
+        console.log(`JSON.stringify(formData)==> ${JSON.stringify(formData)}`)
+      )
+      .then((popTableResult) => popTableResult.json())
+      .then((popTableResultJSON) => {
+        console.log(
+          `JSON.stringify(popTableResultJSON)==> ${JSON.stringify(
+            popTableResultJSON
+          )}`
+        );
+        popTableResponse = popTableResultJSON;
 
-function popTable() {
-  const formData = new FormData();
-  formData.append("popTableTableName", popTableTableName.value);
-  formData.append("popTableFile", popTableFile[0]);
+        utilResponses.set([{ utilResponse: popTableResponse }]);
 
-  // console.log(`formData==> ${formData}`);
-  // console.log(`JSON.stringify(formData)==> ${JSON.stringify(formData)}`);
-
-  fetch("server_routes/rt_MySQL/popTable", {
-    method: "POST",
-    // headers: {
-    //   "Content-Type": "application/json",
-    // },
-    // body: JSON.stringify({
-    //   popTableFile: popTableFile.value,
-    //   popTableTableName: popTableTableName.value,
-    // }),
-    body: formData,
-  })
-    .then(
-      console.log(`JSON.stringify(formData)==> ${JSON.stringify(formData)}`)
-    )
-    .then((popTableResult) => popTableResult.json())
-    .then((popTableResultJSON) => {
-      console.log(
-        `JSON.stringify(popTableResultJSON)==> ${JSON.stringify(
-          popTableResultJSON
-        )}`
-      );
-      popTableResponse = popTableResultJSON;
-
-      utilResponses.set([{ utilResponse: popTableResponse }]);
-
-      // utilResponses.update((currentData) => {
-      //   currentData = [
-      //     {
-      //       utilResponse: Object.values(popTableResponse),
-      //     },
-      //   ];
-      //   return currentData;
-      // });
-    });
+        // utilResponses.update((currentData) => {
+        //   currentData = [
+        //     {
+        //       utilResponse: Object.values(popTableResponse),
+        //     },
+        //   ];
+        //   return currentData;
+        // });
+      });
+  }
 }
 </script>
-
-<!-- <form
-  id="popTable"
-  name="popTable"
-  method="POST"
-  encType="multipart/form-data"
-  accept-charset="UTF-8">
-  <div style="text-align:center">
-    <label for="popTableTableName">Table Name:</label>
-    <input type="text" id="popTableTableName" name="popTableTableName" />
-  </div>
-  <div style="text-align:center">
-    <label for="popTableFile">Upload file:</label>
-    <input type="file" id="popTableFile" name="popTableFile" />
-  </div>
-  <div style="text-align:center">
-    <button type="submit" form="popTable" on:click="{popTable}"
-      >popTable</button>
-  </div>
-</form> -->
 
 <div>
   <div style="text-align:center">
@@ -102,7 +72,7 @@ function popTable() {
       type="file"
       id="popTableFile"
       name="popTableFile"
-      bind:files="{popTableFile}" />
+      bind:this="{popTableFile}" />
   </div>
   <div style="text-align:center">
     <label for="popTableTableName">Table Name</label>
@@ -112,7 +82,7 @@ function popTable() {
       type="text"
       id="popTableTableName"
       name="popTableTableName"
-      bind:value="{popTableTableName}" />
+      bind:this="{popTableTableName}" />
   </div>
   <div style="text-align:center">
     <button on:click="{popTable}">popTable</button>
