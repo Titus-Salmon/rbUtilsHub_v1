e25c07a75b969050c2b27beca6d48037931877a7
tsalm0n <tsalm0n@twc.com>
2021-02-24 21:35:11 -0500

    troubleshoot fetch with file upload

--- src/routes/server_routes/rt_MySQL/test.js
+++ src/routes/server_routes/rt_MySQL/test.js
@@ -21,7 +21,43 @@ export async function post(req, res, next) {
   console.log(`req.body from test.js==> ${req.body}`)
   console.log(`JSON.stringify(req.body) from test.js==> ${JSON.stringify(req.body)}`)
   console.log(`Object.keys(req.body) from test.js==> ${Object.keys(req.body)}`)
-  // console.log(`Object.values(req) from test.js==> ${Object.values(req)}`)
+  console.log(`req.files==> ${req.files}`)
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //   // if (Object.keys(req.files).length === 0) {
+  //   //   return res.status(400).send('No files were uploaded.');
+  //   // }
+
+  //   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  //   let fileToUpload = req.files.popTableFile;
+  //   console.log(`fileToUpload==> ${fileToUpload}`)
+
+  //   // Use the mv() method to place the file somewhere on your server
+  //   fileToUpload.mv(process.cwd() + '/static/csv-to-insert/' + fileToUpload.name, function (err) {
+  //     //might want to try and use the actual UNC path here (\\WEBSERVER...)
+  //     if (err)
+  //       return res.status(500).send(err);
+  //   })
+
+  //   let tableToPopulate = req.body['popTableTableName']
+  //   console.log(`req.body['popTableTableName']==> ${req.body['popTableTableName']}`)
+
+  //   let popTableQuery = `LOAD DATA LOCAL INFILE './static/csv-to-insert/${fileToUpload.name}' INTO TABLE ${tableToPopulate} FIELDS TERMINATED BY ','
+  //      ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;` //"IGNORE 1 LINES" skips the 1st row of the csv (which is the column name line)
+
+  //   connection.query(popTableQuery, (error, response) => {
+  //     if (error) {
+  //       console.log('error===>', error)
+  //     } else {
+  //       console.log('response==>', response);
+  //     }
+  //   }).on('end', function () {
+  //     // all rows have been received
+  //     res.json({
+  //       "response1 from popTable": `~~~~~>> ${process.cwd()}/static/csv-to-insert/${fileName}.csv SAVED <<~~~~~`,
+  //     })
+  //   })
 
   res.json({
     "response from test.js": "response"
