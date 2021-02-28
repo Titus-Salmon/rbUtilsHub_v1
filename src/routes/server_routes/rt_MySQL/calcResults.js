8df05e2783f68a70a4ec1dc4572347072bf6dafe
tsalm0n <tsalm0n@twc.com>
2021-02-28 03:44:36 -0500

    begin calcResults

---
+++ src/routes/server_routes/rt_MySQL/calcResults.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true //MUST HAVE to make more than 1 sql statement in a single query
+})
