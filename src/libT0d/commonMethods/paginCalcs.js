7a5e502676f7ca5def396e326631cfb488204e1b
tsalm0n <tsalm0n@twc.com>
2021-02-28 17:10:38 -0500

    modularizing query methods

--- src/libT0d/commonMethods/paginCalcs.js
+++ src/libT0d/commonMethods/paginCalcs.js
@@ -1,5 +1,5 @@
 async function paginCalcs(totalPages, queryResArr) { //we are hard-coding page length to 100 results per page for now
-  totalPages = Math.ceil(queryResArr.length / 100)
+  return totalPages = Math.ceil(queryResArr.length / 100)
 }
 
 export default paginCalcs
