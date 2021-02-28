8fd14a011a21f34453ed2a8fa683ff03451b6c39
tsalm0n <tsalm0n@twc.com>
2021-02-27 23:09:08 -0500

    add charmProfiles to staging

--- src/components/MySQL/stagedDataModal.svelte
+++ src/components/MySQL/stagedDataModal.svelte
@@ -37,68 +37,75 @@ function calcResults() {
 </script>
 
 {#if $stagingData[0].stagingDataResponse !== null}
-  <!--v-- staged margins ------------------------------------------------------------------------------------->
-  {#if $stagingData[0].stagingDataResponse.stagedMargins}
-    {#each Object.values($stagingData[0].stagingDataResponse.stagedMargins) as stagedMargin}
-      <div style="text-align:center;">
-        <label for="{stagedMargin.dptName}">{stagedMargin.dptName}</label>
-      </div>
-      {#if stagedMargin.nonDefaultMargin}
-        <div style="text-align:center">
-          <input
-            style="background-color: #ff6666; color: black"
-            type="text"
-            id="{stagedMargin.dptName}"
-            name="{stagedMargin.dptName}"
-            bind:value="{stagedMargin.margin}" />
-        </div>
-      {:else}
-        <div style="text-align:center">
-          <input
-            type="text"
-            id="{stagedMargin.dptName}"
-            name="{stagedMargin.dptName}"
-            bind:value="{stagedMargin.margin}" />
-        </div>
+  <div class="flexbox">
+    <div>
+      <!--v-- staged margins ------------------------------------------------------------------------------------->
+      {#if $stagingData[0].stagingDataResponse.stagedMargins}
+        {#each Object.values($stagingData[0].stagingDataResponse.stagedMargins) as stagedMargin}
+          <div style="text-align:center;">
+            <label for="{stagedMargin.dptName}">{stagedMargin.dptName}</label>
+          </div>
+          {#if stagedMargin.nonDefaultMargin}
+            <div style="text-align:center">
+              <input
+                style="background-color: #ff6666; color: black"
+                type="text"
+                id="{stagedMargin.dptName}"
+                name="{stagedMargin.dptName}"
+                bind:value="{stagedMargin.margin}" />
+            </div>
+          {:else}
+            <div style="text-align:center">
+              <input
+                type="text"
+                id="{stagedMargin.dptName}"
+                name="{stagedMargin.dptName}"
+                bind:value="{stagedMargin.margin}" />
+            </div>
+          {/if}
+        {/each}
       {/if}
-    {/each}
-  {/if}
-  <!--^-- staged margins ------------------------------------------------------------------------------------->
+      <!--^-- staged margins ------------------------------------------------------------------------------------->
+    </div>
 
-  <!--v-- staged charms - grocery ------------------------------------------------------------------------------------->
-  {#if $stagingData[0].stagingDataResponse.charmProfiles.grocery}
-    <div style="text-align:center;">
-      <label
-        for="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
-          .lowerCutoffRqdRtl.name}"
-        >{$stagingData[0].stagingDataResponse.charmProfiles.grocery
-          .lowerCutoffRqdRtl.name}</label>
+    <div>
+      <!--v-- staged charms - grocery ------------------------------------------------------------------------------------->
+      {#if $stagingData[0].stagingDataResponse.charmProfiles.grocery}
+        <div style="text-align:center">Grocery Charm Profile</div>
+        <div style="text-align:center;">
+          <label
+            for="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
+              .lowerCutoffRqdRtl.name}"
+            >{$stagingData[0].stagingDataResponse.charmProfiles.grocery
+              .lowerCutoffRqdRtl.name}</label>
+        </div>
+        <div style="text-align:center">
+          <input
+            type="text"
+            id="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
+              .lowerCutoffRqdRtl.name}"
+            name="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
+              .lowerCutoffRqdRtl.name}"
+            bind:value="{$stagingData[0].stagingDataResponse.charmProfiles
+              .grocery.lowerCutoffRqdRtl.dollars}" />
+        </div>
+        {#each $stagingData[0].stagingDataResponse.charmProfiles.grocery.lowercutoffCharms as lowercutoffCharmGroc}
+          <div style="text-align:center;">
+            <label for="{lowercutoffCharmGroc.charmName}"
+              >{lowercutoffCharmGroc.charmName}</label>
+          </div>
+          <div style="text-align:center">
+            <input
+              type="text"
+              id="{lowercutoffCharmGroc.charmName}"
+              name="{lowercutoffCharmGroc.charmName}"
+              bind:value="{lowercutoffCharmGroc.cents}" />
+          </div>
+        {/each}
+      {/if}
+      <!--^-- staged charms  - grocery------------------------------------------------------------------------------------->
     </div>
-    <div style="text-align:center">
-      <input
-        type="text"
-        id="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
-          .lowerCutoffRqdRtl.name}"
-        name="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
-          .lowerCutoffRqdRtl.name}"
-        bind:value="{$stagingData[0].stagingDataResponse.charmProfiles.grocery
-          .lowerCutoffRqdRtl.dollars}" />
-    </div>
-    {#each $stagingData[0].stagingDataResponse.charmProfiles.grocery.lowercutoffCharms as lowercutoffCharmGroc}
-      <div style="text-align:center;">
-        <label for="{lowercutoffCharmGroc.charmName}"
-          >{lowercutoffCharmGroc.charmName}</label>
-      </div>
-      <div style="text-align:center">
-        <input
-          type="text"
-          id="{lowercutoffCharmGroc.charmName}"
-          name="{lowercutoffCharmGroc.charmName}"
-          bind:value="{lowercutoffCharmGroc.cents}" />
-      </div>
-    {/each}
-  {/if}
-  <!--^-- staged charms  - grocery------------------------------------------------------------------------------------->
+  </div>
 
   <div style="text-align:center">
     <button on:click="{calcResults}">calcResults</button>
