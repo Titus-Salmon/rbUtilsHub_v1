69811c4026fe3a02a856214d94194657678c565c
tsalm0n <tsalm0n@twc.com>
2021-03-02 23:20:03 -0500

    form data handling from stagedDataModal

--- src/libT0d/defaultMargs/defaultMargs.js
+++ src/libT0d/defaultMargs/defaultMargs.js
@@ -1,110 +1,110 @@
 let defaultMargArr = [{
-    "dptName": "Beer & Alcohol",
+    "dptName": "beerAlc",
     "dptNumb": "54",
     "margin": "20"
   },
   {
-    "dptName": "Body Care",
+    "dptName": "bodyCare",
     "dptNumb": "152",
     "margin": "45"
   },
   {
-    "dptName": "Books",
+    "dptName": "books",
     "dptNumb": "9",
     "margin": "40"
   },
   {
-    "dptName": "Bulk",
+    "dptName": "bulk",
     "dptNumb": "19",
     "margin": "45"
   },
   {
-    "dptName": "Bulk & Herb Prepack",
+    "dptName": "bulkHerbPrepack",
     "dptNumb": "30",
     "margin": "45"
   },
   {
-    "dptName": "CBD - Grocery",
+    "dptName": "cbdGroc",
     "dptNumb": "175",
     "margin": "45"
   },
   {
-    "dptName": "CBD - Supplements",
+    "dptName": "cbdSupp",
     "dptNumb": "176",
     "margin": "50"
   },
   {
-    "dptName": "CBD - Topicals",
+    "dptName": "cbdTop",
     "dptNumb": "177",
     "margin": "45"
   },
   {
-    "dptName": "Consignments",
+    "dptName": "consignments",
     "dptNumb": "148",
     "margin": "20"
   },
   {
-    "dptName": "General Merchandise",
+    "dptName": "genMerch",
     "dptNumb": "150",
     "margin": "38"
   },
   {
-    "dptName": "Gift Items",
+    "dptName": "gift",
     "dptNumb": "13",
     "margin": "50"
   },
   {
-    "dptName": "Grab & Go",
+    "dptName": "grabGo",
     "dptNumb": "62",
     "margin": "33"
   },
   {
-    "dptName": "Grocery",
+    "dptName": "grocery",
     "dptNumb": "25",
     "margin": "38"
   },
   {
-    "dptName": "Grocery - Local",
+    "dptName": "groceryLocal",
     "dptNumb": "179",
     "margin": "33"
   },
   {
-    "dptName": "Grocery - Local Meat",
+    "dptName": "groceryLocalMeat",
     "dptNumb": "38",
     "margin": "25"
   },
   {
-    "dptName": "HBA",
+    "dptName": "hba",
     "dptNumb": "12",
     "margin": "45"
   },
   {
-    "dptName": "Herbs & Homeopathic",
+    "dptName": "herbsHomeo",
     "dptNumb": "158",
     "margin": "50" //changed from 40% per Andrea 20200715  
   },
   {
-    "dptName": "LifeBar",
+    "dptName": "lifeBar",
     "dptNumb": "80",
     "margin": "0"
   },
   {
-    "dptName": "Other",
+    "dptName": "other",
     "dptNumb": "151",
     "margin": "45"
   },
   {
-    "dptName": "Pet Supplies",
+    "dptName": "petSupplies",
     "dptNumb": "183",
     "margin": "38"
   },
   {
-    "dptName": "Refrigerated",
+    "dptName": "refrigerated",
     "dptNumb": "155",
     "margin": "33"
   },
   {
-    "dptName": "Vitamins & Supplements",
+    "dptName": "vitSupp",
     "dptNumb": "157",
     "margin": "50" //changed from 40% per Andrea 20200715  
   }
