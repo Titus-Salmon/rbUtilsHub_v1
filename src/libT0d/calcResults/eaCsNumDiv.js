569f092399dd8f14557da6e0ef8027d95cfa70a0
tsalm0n <tsalm0n@twc.com>
2021-03-03 20:50:45 -0500

    proto wholesale updater logic

--- src/libT0d/calcResults/eaCsNumDiv.js
+++ src/libT0d/calcResults/eaCsNumDiv.js
@@ -1,9 +1,9 @@
 let unitCost;
 
-function eaCsNumDiv(n, reqBody) {
+function eaCsNumDiv(n) {
   let eaCsNum = queryResArr[n]['oup_name'].split('-')[1] //isolate the number portion of EA/CS-##
   let venCost = queryResArr[n][`${venCatPrefix}_cost`] //get cost from vendor catalog
-  if (reqBody.eaNumDivide === 'yes' || reqBody.csNumDivide === 'yes') {
+  if (eaNumDivide === 'yes' || csNumDivide === 'yes') {
     //domathToGetToUnitCost
     unitCost = (venCost / eaCsNum) - (venCost / eaCsNum) * discoMulti
   } else {
