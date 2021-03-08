d0328cd487e1949cc3c8127eeecb3e1f32f48cfc
tsalm0n <tsalm0n@twc.com>
2021-03-07 19:56:05 -0500

    show/hide staging results mods

--- src/components/UI/showHideStagedDataBtn.svelte
+++ src/components/UI/showHideStagedDataBtn.svelte
@@ -2,7 +2,9 @@
 function showHidestagedData() {
   let stagedDataModal = document.getElementById("stagedDataModal");
 
-  if (localStorage.getItem("showHidestagedDataClickCounter") == 0) {
+  localStorage.setItem("showHidestagedDataClickCounter", 0);
+
+  if (localStorage.getItem("showHidestagedDataClickCounter") === 0) {
     //this is kind of an odd bird, because it concatenates the result after adding 1
     //to produce 01, instead of 1, but it works. Maybe something to spend some time on down the road, but the way it's currently written
     //is kind of cool.
