a397ded5936868966809de061b091dbc9f6da3c4
tsalm0n <tsalm0n@twc.com>
2021-03-11 22:05:42 -0500

    wholesaleCalcs work

--- src/libT0d/calcResults/wholesaleCalcs.js
+++ src/libT0d/calcResults/wholesaleCalcs.js
@@ -101,7 +101,67 @@ function wholesaleCalcs(reqBody, queryResArr, populated_imw_arr, modifiedQueryRe
       imwToPop['ovr'] = ovr
 
       populated_imw_arr.push(imwToPop)
-      modifiedQueryResArr.push(queryResArr[i])
+      // modifiedQueryResArr.push(queryResArr[i])
+
+      ////////////////////////////////////////////////////////////////////////////////////
+      // //v//ADD numPkgs, csPkgMltpl, ovr, appliedWSdisco, etc.
+      // //v//to modifiedQueryResArr, in order to show for review purposes on frontend
+      let reviewObj = {}
+      reviewObj['upc'] = `${queryResArr[i]['inv_ScanCode']}`
+      reviewObj['cpltSKU'] = `${queryResArr[i]['ord_supplierstocknumber']}`
+      reviewObj['ediSKU'] = `${queryResArr[i][`${reqBody.venCatPrefix}_sku`]}`
+      if ((reviewObj['cpltSKU']).toLowerCase() !== (reviewObj['ediSKU']).toLowerCase()) { //flag SKU mismatches, if they occur
+        reviewObj['skuMismatch'] = `${reviewObj['upc']}`
+      } else {
+        reviewObj['skuMismatch'] = ""
+      }
+      reviewObj['deptName'] = `${queryResArr[i]['dpt_name']}`
+      reviewObj['rcptAlias'] = `${queryResArr[i]['inv_receiptalias']}`
+      reviewObj['brand'] = `${queryResArr[i]['brd_name']}`
+      reviewObj['size'] = `${queryResArr[i]['inv_size']}`
+      reviewObj['lastCost'] = `${queryResArr[i]['inv_lastcost']}`
+      reviewObj['ediCost'] = `${queryResArr[i][`${reqBody.venCatPrefix}_cost`]}`
+      reviewObj['WS_disco'] = `${reqBody.ongDisco_WS}`
+      reviewObj['ediCostMod'] = `${unitCost}`
+      // reviewObj['Rtl_disco'] = `${reqBody.ongDisco_Rtl}`
+      reviewObj['sib_baseprice'] = `${queryResArr[i]['sib_baseprice']}`
+      // reviewObj['reqdRtl'] = `${reqdRtl}`
+      // reviewObj['charm'] = `${charm}`
+      reviewObj['ediPrice'] = `${queryResArr[i][`${reqBody.venCatPrefix}_msrp`]}`
+      reviewObj['idealMarg'] = `${queryResArr[i]['sib_idealmargin']}`
+      reviewObj['actlMarg'] = `${queryResArr[i]['actlMarg']}`
+      // reviewObj['appliedMargin'] = `${marginToApply*100}`
+      reviewObj['altID'] = `${queryResArr[i]['asc_scancode']}`
+      reviewObj['altRcptAlias'] = `${queryResArr[i]['asc_receiptalias']}`
+      reviewObj['pkgQnt'] = `${queryResArr[i]['asc_quantity']}` //***this is pkg qty for ALD ID***
+      reviewObj['splrID'] = `${queryResArr[i]['ven_companyname']}`
+      reviewObj['unit'] = `${queryResArr[i]['oup_name']}`
+      reviewObj['numPkgs'] = `${nmPk}`
+      reviewObj['csPkgMltpl'] = `${csPk}`
+      reviewObj['ovr'] = `${ovr}`
+      reviewObj['pf1'] = `${queryResArr[i]['pi1_description']}`
+      reviewObj['pf2'] = `${queryResArr[i]['pi2_description']}`
+      // reviewObj['pf3'] = ""
+      // reviewObj['pf4'] = ""
+      // reviewObj['pf5'] = ""
+      // reviewObj['pf6'] = ""
+      // reviewObj['pf7'] = ""
+      // reviewObj['pf8'] = ""
+
+      modifiedQueryResArr.push(reviewObj) //this holds data for displaying REVIEW results
+      //AND we are adding come calcResults as well (see above), for review purposes
+      //we need some way of reordering our columns for the review array (modifiedQueryResArr), so why not
+      //just overwrite your cache with the review results, and everything should fall in place
+      //V// CACHE QUERY RESULTS IN BACKEND (for saveToCSV, and possibly other things)//////////////////////////////////////////////////////////////////////////////
+      queryResArrCache.set('queryResArrCache_key', modifiedQueryResArr)
+      console.log(`queryResArrCache['data']['queryResArrCache_key']['v'].length==> ${queryResArrCache['data']['queryResArrCache_key']['v'].length}`)
+      console.log(`queryResArrCache['data']['queryResArrCache_key']['v'][0]==> ${queryResArrCache['data']['queryResArrCache_key']['v'][0]}`)
+      console.log(`JSON.stringify(queryResArrCache['data']['queryResArrCache_key']['v'][0])==> ${JSON.stringify(queryResArrCache['data']['queryResArrCache_key']['v'][0])}`)
+      //^// CACHE QUERY RESULTS IN BACKEND //////////////////////////////////////////////////////////////////////////////
+
+      // //^//ADD numPkgs, csPkgMltpl, ovr, appliedWSdisco, etc.
+      // //^//to modifiedQueryResArr, in order to show for review purposes on frontend
+
     }
   }
   calcResStatus = `There were ${populated_imw_arr.length} items in need of wholesale update. 
