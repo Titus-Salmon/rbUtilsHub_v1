f17b85b0b0a8ade968ad2bae22ccbdd05e7703f5
tsalm0n <tsalm0n@twc.com>
2021-03-15 08:59:49 -0400

    tableHighlight troubleshoot

--- src/components/queryResTbls/queryResultsTable1.svelte
+++ src/components/queryResTbls/queryResultsTable1.svelte
@@ -138,8 +138,8 @@ th {
 
       {#each Object.values($tableData) as row}
         <tr>
-          {#if Object.keys(row) === "ediCostMod" || Object.keys(row) === "lastCost"}
-            {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
+          {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
+            {#if Object.keys(row) === "ediCostMod" || Object.keys(row) === "lastCost"}
               {#each Object.values(row) as cell}
                 <td style="background-color:#ffb3ca; color: black">{cell}</td>
               {/each}
@@ -153,6 +153,9 @@ th {
               <td>{cell}</td>
             {/each}
           {/if}
+          <!-- {#each Object.values(row) as cell}
+            <td>{cell}</td>
+          {/each} -->
         </tr>
       {/each}
       <!-- if there is any data in the table store, call tableHighlight -->
