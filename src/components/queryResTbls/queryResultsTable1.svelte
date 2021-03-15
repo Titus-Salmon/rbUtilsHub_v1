a6d1d022add00584c67bdc52686461bc175f25f8
tsalm0n <tsalm0n@twc.com>
2021-03-15 00:49:09 -0400

    tableHighlight troubleshoot

--- src/components/queryResTbls/queryResultsTable1.svelte
+++ src/components/queryResTbls/queryResultsTable1.svelte
@@ -126,10 +126,9 @@ th {
         </tr>
       {/each}
       <!-- if there is any data in the table store, call tableHighlight -->
-      <!-- {#if Object.keys($tableData[0]).length > 0}
+      {#if Object.keys($tableData[0]).length > 0}
         {tableHighlight()}
-      {/if} -->
-      {tableHighlight()}
+      {/if}
     </tbody>
   </table>
 </body>
