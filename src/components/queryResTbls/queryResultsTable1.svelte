03c4aa482bd682766b1e1a6e752a19c8d71b6c5f
tsalm0n <tsalm0n@twc.com>
2021-03-15 05:45:59 -0400

    tableHighlight troubleshoot

--- src/components/queryResTbls/queryResultsTable1.svelte
+++ src/components/queryResTbls/queryResultsTable1.svelte
@@ -130,8 +130,9 @@ th {
       {#each Object.values($tableData) as row}
         <tr>
           {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
-            {#each Object.values(row) as cell}
-              <td style="background-color: #ffb3ca; color: black">{cell}</td>
+            {#each Object.values(row["ediCostMod"]) as ediCostMod}
+              <td style="background-color: #ffb3ca; color: black"
+                >{ediCostMod}</td>
             {/each}
           {/if}
         </tr>
