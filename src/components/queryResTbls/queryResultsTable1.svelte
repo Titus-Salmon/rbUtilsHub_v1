54cc64782c1277351f55bbe3cae1747fa333a088
tsalm0n <tsalm0n@twc.com>
2021-03-15 08:17:24 -0400

    tableHighlight troubleshoot

--- src/components/queryResTbls/queryResultsTable1.svelte
+++ src/components/queryResTbls/queryResultsTable1.svelte
@@ -138,22 +138,22 @@ th {
 
       {#each Object.values($tableData) as row}
         <tr>
-          {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
-            <!-- {console.log(`row["ediCostMod"]==> ${row["ediCostMod"]}`)}
+          <!-- {console.log(`row["ediCostMod"]==> ${row["ediCostMod"]}`)}
             {console.log(`row["lastCost"]==> ${row["lastCost"]}`)} -->
-            {#each Object.values($tableData) as cell}
-              {#if Object.keys(cell) === "ediCostMod" || Object.keys(cell) === "lastCost"}
+          {#each Object.values($tableData) as cell}
+            {#if Object.keys(cell) === "ediCostMod" || Object.keys(cell) === "lastCost"}
+              {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
                 <td style="background-color:#ffb3ca; color: black"
                   >{Object.values(cell)}</td>
               {:else}
                 <td>{Object.values(cell)}</td>
               {/if}
-            {/each}
+            {/if}
           {:else}
             {#each Object.values(row) as cell}
               <td>{cell}</td>
             {/each}
-          {/if}
+          {/each}
         </tr>
       {/each}
       <!-- if there is any data in the table store, call tableHighlight -->
