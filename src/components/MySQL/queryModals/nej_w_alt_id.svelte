727b14118ca956a76c9bd7e6d05f80150bf6c963
tsalm0n <tsalm0n@twc.com>
2021-03-23 07:42:17 -0400

    add alternate ID handling for bulk updates

--- src/components/MySQL/queryModals/nej_w_alt_id.svelte
+++ src/components/MySQL/queryModals/nej_w_alt_id.svelte
@@ -2,19 +2,19 @@
 let mySqlQueryText;
 let queryText = `
 SELECT 
-edi_table.*, 
-
 nhcrt.ri_t0d, nhcrt.inv_ScanCode, nhcrt.ord_supplierstocknumber, nhcrt.inv_name, nhcrt.inv_size, nhcrt.inv_receiptalias, nhcrt.inv_datecreated, 
 nhcrt.ord_quantityinorderunit, nhcrt.oup_name, nhcrt.sto_number, nhcrt.brd_name, nhcrt.dpt_name, nhcrt.dpt_number, nhcrt.sib_idealmargin, 
 nhcrt.actlMarg, nhcrt.ven_companyname, nhcrt.ven_code, nhcrt.asc_scancode, nhcrt.asc_receiptalias, nhcrt.asc_quantity, nhcrt.inv_lastcost, 
-nhcrt.sib_baseprice, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, nhcrt.inv_powerfield4 
+nhcrt.sib_baseprice, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, nhcrt.inv_powerfield4, 
 
-FROM ediTableName
+edi_table.* 
 
-edi_table JOIN nhcrtTableName nhcrt 
-ON edi_table.ediPrefix_upc 
-WHERE edi_table.ediPrefix_upc = nhcrt.inv_ScanCode
-OR edi_table.ediPrefix_upc = nhcrt.asc_scancode
+FROM nhcrtTableName 
+
+nhcrt JOIN ediTableName edi_table 
+ON nhcrt.inv_ScanCode 
+WHERE nhcrt.inv_ScanCode = edi_table.ediPrefix_upc 
+OR nhcrt.asc_scancode = edi_table.ediPrefix_upc
 
 ORDER BY nhcrt.dpt_name, nhcrt.pi1_description, nhcrt.pi1_description;`;
 </script>
--- src/components/MySQL/querySelectDropdown.svelte
+++ src/components/MySQL/querySelectDropdown.svelte
@@ -13,20 +13,20 @@ let selected;
 <div style="padding-left: 6rem">
   <label for="querySelect">Select Query:</label>
   <select name="querySelect" id="querySelect" bind:value="{selected}">
-    <option value="nej">nej</option>
     <option value="nej_w_alt_id">nej_w_alt_id</option>
+    <option value="nej">nej</option>
     <option value="showTables">showTables</option>
     <option value="spins1">spins1</option>
     <option value="compEdiNhcrt">compEdiNhcrt</option>
   </select>
 </div>
 
-{#if selected === "nej"}
-  <NejQueryModal />
-{/if}
-
 {#if selected === "nej_w_alt_id"}
   <Nej_w_alt_id />
+{/if}
+
+{#if selected === "nej"}
+  <NejQueryModal />
 {/if}
 
 {#if selected === "showTables"}
