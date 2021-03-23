542fe91b3ca7eccddc3cdcec524ce986b725f7c2
tsalm0n <tsalm0n@twc.com>
2021-03-23 05:09:47 -0400

    add alternate ID handling for bulk updates

--- src/components/MySQL/queryModals/nej_w_alt_id.svelte
+++ src/components/MySQL/queryModals/nej_w_alt_id.svelte
@@ -2,7 +2,7 @@
 let mySqlQueryText;
 let queryText = `
 SELECT 
-edi_table.* 
+edi_table.*, 
 
 nhcrt.ri_t0d, nhcrt.inv_ScanCode, nhcrt.ord_supplierstocknumber, nhcrt.inv_name, nhcrt.inv_size, nhcrt.inv_receiptalias, nhcrt.inv_datecreated, 
 nhcrt.ord_quantityinorderunit, nhcrt.oup_name, nhcrt.sto_number, nhcrt.brd_name, nhcrt.dpt_name, nhcrt.dpt_number, nhcrt.sib_idealmargin, 
