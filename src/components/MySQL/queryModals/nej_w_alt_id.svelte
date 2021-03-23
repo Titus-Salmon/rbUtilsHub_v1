d405d9f1d3ac8e7b7e4f793be4bb17e14b135b37
tsalm0n <tsalm0n@twc.com>
2021-03-23 05:16:29 -0400

    add alternate ID handling for bulk updates

--- src/components/MySQL/queryModals/nej_w_alt_id.svelte
+++ src/components/MySQL/queryModals/nej_w_alt_id.svelte
@@ -7,7 +7,7 @@ edi_table.*,
 nhcrt.ri_t0d, nhcrt.inv_ScanCode, nhcrt.ord_supplierstocknumber, nhcrt.inv_name, nhcrt.inv_size, nhcrt.inv_receiptalias, nhcrt.inv_datecreated, 
 nhcrt.ord_quantityinorderunit, nhcrt.oup_name, nhcrt.sto_number, nhcrt.brd_name, nhcrt.dpt_name, nhcrt.dpt_number, nhcrt.sib_idealmargin, 
 nhcrt.actlMarg, nhcrt.ven_companyname, nhcrt.ven_code, nhcrt.asc_scancode, nhcrt.asc_receiptalias, nhcrt.asc_quantity, nhcrt.inv_lastcost, 
-nhcrt.sib_baseprice, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, nhcrt.inv_powerfield4, 
+nhcrt.sib_baseprice, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, nhcrt.inv_powerfield4 
 
 FROM ediTableName
 
