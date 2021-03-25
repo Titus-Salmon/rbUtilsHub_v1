10eaf3b6658f13085af5d79e6e153f81b6bdea72
tsalm0n <tsalm0n@twc.com>
2021-03-25 01:11:08 -0400

    ws & rtl table results highlighting troubleshoot

--- src/components/queryResTbls/highlightLogic/wsLogic.svelte
+++ src/components/queryResTbls/highlightLogic/wsLogic.svelte
@@ -4,19 +4,18 @@ import tableData from "../../../stores/dynamicTables/tableData1";
 
 {#each $tableData as row}
   <tr>
-    {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35 && Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) < 0.5}
+    {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.5}
       {#each Object.keys(row) as colName}
         {#if colName === "ediCostMod" || colName === "lastCost"}
-          <td style="background-color:#ffb3ca; color: black">{row[colName]}</td>
+          <td style="background-color:#ff8533; color: black">{row[colName]}</td>
         {:else}
           <td>{row[colName]}</td>
         {/if}
       {/each}
-    {/if}
-    {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) >= 0.5}
+    {:else if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35}
       {#each Object.keys(row) as colName}
         {#if colName === "ediCostMod" || colName === "lastCost"}
-          <td style="background-color:#ff8533; color: black">{row[colName]}</td>
+          <td style="background-color:#ffb3ca; color: black">{row[colName]}</td>
         {:else}
           <td>{row[colName]}</td>
         {/if}
