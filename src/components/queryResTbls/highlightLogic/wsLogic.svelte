1befe246af7bbd74a09269fb67c826af0f1f6e00
tsalm0n <tsalm0n@twc.com>
2021-03-24 23:22:35 -0400

    ws & rtl table results highlighting troubleshoot

--- src/components/queryResTbls/highlightLogic/wsLogic.svelte
+++ src/components/queryResTbls/highlightLogic/wsLogic.svelte
@@ -6,29 +6,24 @@ import tableData from "../../../stores/dynamicTables/tableData1";
   <tr>
     {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.35 && Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) <= 0.5}
       {#each Object.keys(row) as colName}
-        {#if colName === "ediCostMod"}
+        {#if colName === "ediCostMod" && colName === "lastCost"}
           <td style="background-color:#ffb3ca; color: black">{row[colName]}</td>
-        {:else if colName === "lastCost"}
-          <td style="background-color:#ffb3ca; color: black">{row[colName]}</td>
-        {:else}
-          <td>{row[colName]}</td>
         {/if}
       {/each}
     {/if}
     {#if Math.abs((row["ediCostMod"] - row["lastCost"]) / row["ediCostMod"]) > 0.5}
       {#each Object.keys(row) as colName}
-        {#if colName === "ediCostMod"}
+        {#if colName === "ediCostMod" && colName === "lastCost"}
           <td style="background-color:#ff8533; color: black">{row[colName]}</td>
-        {:else if colName === "lastCost"}
-          <td style="background-color:#ff8533; color: black">{row[colName]}</td>
-        {:else}
-          <td>{row[colName]}</td>
         {/if}
       {/each}
-    {:else}
-      {#each Object.values(row) as cell}
-        <td>{cell}</td>
-      {/each}
     {/if}
+    {#each Object.keys(row) as colName}
+      {#if colName !== "ediCostMod" && colName !== "lastCost" && colName === "ediCostMod" && colName === "lastCost"}
+        {#each Object.values(row) as cell}
+          <td>{cell}</td>
+        {/each}
+      {/if}
+    {/each}
   </tr>
 {/each}
