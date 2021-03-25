8c9ff652c9b1eb31a96f3e0ae6121139367e018f
tsalm0n <tsalm0n@twc.com>
2021-03-24 23:08:02 -0400

    ws & rtl table results highlighting troubleshoot

--- src/components/queryResTbls/highlightLogic/rtlLogic.svelte
+++ src/components/queryResTbls/highlightLogic/rtlLogic.svelte
@@ -4,23 +4,23 @@ import tableData from "../../../stores/dynamicTables/tableData1";
 
 {#each $tableData as row}
   <tr>
-    {#if Math.abs((row["charm"] - row["sib_baseprice"]) / row["charm"]) > 0.5}
-      {#each Object.keys(row) as colName}
-        {#if colName === "charm"}
-          <td style="background-color:#ff0000; color: black">{row[colName]}</td>
-        {:else if colName === "sib_baseprice"}
-          <td style="background-color:#ff0000; color: black">{row[colName]}</td>
-        {:else}
-          <td>{row[colName]}</td>
-        {/if}
-      {/each}
-    {/if}
     {#if Math.abs((row["charm"] - row["sib_baseprice"]) / row["charm"]) > 0.35}
       {#each Object.keys(row) as colName}
         {#if colName === "charm"}
           <td style="background-color:#ffdb4b; color: black">{row[colName]}</td>
         {:else if colName === "sib_baseprice"}
           <td style="background-color:#ffdb4b; color: black">{row[colName]}</td>
+        {:else}
+          <td>{row[colName]}</td>
+        {/if}
+      {/each}
+    {/if}
+    {#if Math.abs((row["charm"] - row["sib_baseprice"]) / row["charm"]) > 0.5}
+      {#each Object.keys(row) as colName}
+        {#if colName === "charm"}
+          <td style="background-color:#ff0000; color: black">{row[colName]}</td>
+        {:else if colName === "sib_baseprice"}
+          <td style="background-color:#ff0000; color: black">{row[colName]}</td>
         {:else}
           <td>{row[colName]}</td>
         {/if}
