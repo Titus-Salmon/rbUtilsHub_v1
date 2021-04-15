6415fec4a5df2d0f992e2a6e9f5bedf9d7789c38
tsalm0n <tsalm0n@twc.com>
2021-04-15 01:50:16 -0400

    add logic to v_inventorymasterquery for filtering result set based on actual margin range

--- src/libT0d/T-SQL/vInvMasterQuery.svelte
+++ src/libT0d/T-SQL/vInvMasterQuery.svelte
@@ -7,8 +7,8 @@ SELECT
 inv_ScanCode, ord_supplierstocknumber, inv_name, inv_size, inv_receiptalias, inv_datecreated, inv_default, ord_quantityinorderunit, oup_name, 
 sto_number, brd_name, dpt_name, dpt_number, wgt_name, sib_idealMargin, ven_companyname, ven_code, asc_scancode, asc_receiptalias, asc_quantity, 
 convert(varchar(10), inv_lastreceived, 120), convert(varchar(10), inv_lastsold, 120), inv_lastcost, sib_baseprice, inv_onhand, inv_onorder, 
-inv_intransit, inv_memo, pi1_Description, pi2_Description, pi3_Description, pi4_Description, inv_powerfield1, inv_powerfield2, inv_powerfield3, 
-inv_powerfield4 
+inv_intransit, inv_memo, inv_discontinued, pi1_Description, pi2_Description, pi3_Description, pi4_Description, inv_powerfield1, inv_powerfield2, 
+inv_powerfield3, inv_powerfield4 
 FROM
 catapult.ecrs.v_InventoryMaster 
 WHERE trim(ven_companyname) IN ('EDI-ALOE')
