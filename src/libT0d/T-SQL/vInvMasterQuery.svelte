5494b83ef4587166eccd5ae7e478b6990430a962
tsalm0n <tsalm0n@twc.com>
2021-04-24 17:37:52 -0400

    modify vInvMaster query to exclude Cdrf and discod items

--- src/libT0d/T-SQL/vInvMasterQuery.svelte
+++ src/libT0d/T-SQL/vInvMasterQuery.svelte
@@ -12,7 +12,9 @@ inv_powerfield3, inv_powerfield4
 FROM
 catapult.ecrs.v_InventoryMaster 
 WHERE trim(ven_companyname) IN ('EDI-ALOE')
-AND trim(dpt_number) != '999999' ORDER BY dpt_name, pi1_Description, pi2_Description
+AND trim(inv_discontinued) = 0
+AND trim(sto_number) != 'Cdrf'
+AND trim(dpt_number) != '999999' ORDER BY dpt_name, pi1_Description, pi2_Description, inv_ScanCode
 `;
 
 let actlMargRangeLessVal;
