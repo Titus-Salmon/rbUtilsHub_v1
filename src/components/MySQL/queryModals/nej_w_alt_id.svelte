d397376e28b6490f0f2bc9cfe7dab0a7d39880a9
tsalm0n <tsalm0n@twc.com>
2021-04-24 17:54:29 -0400

    modify vInvMaster query to exclude Cdrf and discod items

--- src/components/MySQL/queryModals/nej_w_alt_id.svelte
+++ src/components/MySQL/queryModals/nej_w_alt_id.svelte
@@ -5,7 +5,8 @@ SELECT
 nhcrt.ri_t0d, nhcrt.inv_ScanCode, nhcrt.ord_supplierstocknumber, nhcrt.inv_name, nhcrt.inv_size, nhcrt.inv_receiptalias, nhcrt.inv_datecreated, 
 nhcrt.ord_quantityinorderunit, nhcrt.oup_name, nhcrt.sto_number, nhcrt.brd_name, nhcrt.dpt_name, nhcrt.dpt_number, nhcrt.sib_idealmargin, 
 nhcrt.actlMarg, nhcrt.ven_companyname, nhcrt.ven_code, nhcrt.asc_scancode, nhcrt.asc_receiptalias, nhcrt.asc_quantity, nhcrt.inv_lastcost, 
-nhcrt.sib_baseprice, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, nhcrt.inv_powerfield4, 
+nhcrt.sib_baseprice, nhcrt.inv_discontinued, nhcrt.pi1_description, nhcrt.pi2_description, nhcrt.pi3_description, nhcrt.inv_powerfield3, 
+nhcrt.inv_powerfield4, 
 
 edi_table.* 
 
@@ -16,7 +17,7 @@ ON nhcrt.inv_ScanCode
 WHERE nhcrt.inv_ScanCode = edi_table.ediPrefix_upc 
 OR nhcrt.asc_scancode = edi_table.ediPrefix_upc
 
-ORDER BY nhcrt.dpt_name, nhcrt.pi1_description, nhcrt.pi1_description;`;
+ORDER BY nhcrt.dpt_name, nhcrt.pi1_description, nhcrt.pi1_description, nhcrt.inv_ScanCode;`;
 </script>
 
 <div style="text-align:center">
