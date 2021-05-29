ce763adc53328ce04134e7ddde9409595270e715
tsalm0n <tsalm0n@twc.com>
2021-05-29 01:53:59 -0400

    stockFilterUPC

---
+++ src/routes/pages/pg_MySQL/stockFilterUPC.svelte
@@ -0,0 +1,125 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import QueryResultsTable1 from "../../../components/queryResTbls/queryResultsTable1.svelte";
+  import PaginUI from "../../../components/UI/paginUI.svelte";
+  // import paginData from "../../stores/pagination/st_pagination1";
+  import tableData from "../../../../stores/dynamicTables/tableData1";
+  import utilResponses from "../../../stores/utilResponses/st_utilResponses";
+
+  import SaveToXLSX from "../../../libT0d/saveToXLSX.svelte";
+
+  import { Jumper } from "svelte-loading-spinners";
+
+  let isLoading = false;
+  let tableName;
+  function stockFilterUPC() {
+    isLoading = true;
+
+    fetch("server_routes/rt_MySQL/stockFilterUPC", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tableName: tableName.value,
+      }),
+    })
+      .then((queryRes) => queryRes.json())
+      .then((queryResJSON) => {
+        tableData.set(queryResJSON.stockFilterResultsSplitParsedArr);
+        console.log(
+          `JSON.stringify(queryResJSON.stockFilterResultsSplitParsedArr)==> ${JSON.stringify(
+            queryResJSON.stockFilterResultsSplitParsedArr
+          )}`
+        ); //passing backend response to frontend "Store" & we are overwriting the "Store" with set()
+        // console.log(
+        //   `JSON.stringify($paginData) before update from stockFilterUPC()==> ${JSON.stringify(
+        //     $paginData
+        //   )}`
+        // );
+        // paginData.update((currentData) => {
+        //   currentData = [
+        //     {
+        //       totalPages: queryResJSON.totalPages,
+        //       currentPage: queryResJSON.currentPage,
+        //       nextPage: queryResJSON.nextPage,
+        //       prevPage: queryResJSON.prevPage,
+        //     },
+        //   ];
+        //   return currentData;
+        // });
+        // console.log(
+        //   `JSON.stringify($paginData) after update from stockFilterUPC()==> ${JSON.stringify(
+        //     $paginData
+        //   )}`
+        // );
+        if (queryResJSON.error) {
+          alert(`err from stockFilterUPC.svelte==> ${queryResJSON.error}`);
+        }
+
+        isLoading = false;
+      })
+      .catch((err) => {
+        alert(`err from stockFilterUPC.svelte==> ${err}`);
+        isLoading = false;
+      });
+  }
+</script>
+
+<style></style>
+
+<div style="padding: 0 1rem">
+  <!-- //////////////table name//////////////////////////// -->
+  <div style="text-align:center;">
+    <label for="tableName">nhcrt Table name for Stock Filter</label>
+  </div>
+  <div style="text-align:center">
+    <input
+      type="text"
+      id="tableName"
+      name="tableName"
+      required
+      bind:this="{tableName}" />
+  </div>
+
+  {#if isLoading}
+    <div class="flexbox" margin="1rem">
+      <Jumper size="60" color="#FF3E00" unit="px" duration="1s" />
+    </div>
+  {/if}
+
+  <!-- //////////////button//////////////////////////// -->
+  <div style="text-align:center">
+    <button on:click="{stockFilterUPC}">stockFilterUPC</button>
+  </div>
+</div>
+
+<!--v-- only show inputs & buttons here if tableData store has been populated with query results -->
+<!--v-- NOTE: you must use the $ to access the tableData store -->
+{#if Object.keys($tableData).length > 0}
+  <div class="flexbox">
+    <SaveToXLSX />
+  </div>
+{/if}
+<!--^-- only show inputs & buttons here if tableData store has been populated with query results -->
+
+<div>
+  {#if $utilResponses[0].utilResponse !== null}
+    <p style="text-align:center; color: var(--element6)">
+      {Object.values($utilResponses[0].utilResponse)}
+    </p>
+  {/if}
+</div>
+
+<!-- {#if $paginData[0].totalPages !== null}
+  <p style="text-align:center; margin: 0">
+    Page {$paginData[0].currentPage} of {$paginData[0].totalPages}
+  </p>
+  <PaginUI />
+{/if} -->
+
+{#if $tableData[0] !== undefined && $tableData[0] !== null}
+  {#if Object.keys($tableData[0]).length > 0}
+    <QueryResultsTable1 />
+  {/if}
+{/if}
---
+++ src/routes/server_routes/rt_MySQL/stockFilterUPC.js
@@ -0,0 +1,266 @@
+const mysql = require("mysql");
+const cacheMainStockFilter = require("../../../nodeCacheStuff/cache1");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
+export async function post(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+
+  let tableName = req.body.tableName;
+
+  let stockFilterResultsCacheChecker = cacheMainStockFilter.get(
+    "stockFilterResultsCache_key"
+  );
+  if (stockFilterResultsCacheChecker !== undefined) {
+    //clear stockFilterResultsCache_key if it exists
+    cacheMainStockFilter.del("stockFilterResultsCache_key");
+  }
+
+  let sfRsINDstocked = [];
+  let sfRsIND_NOTstocked = [];
+  let sfRsSMstocked = [];
+  let sfRsSM_NOTstocked = [];
+  let sfRsMTstocked = [];
+  let sfRsMT_NOTstocked = [];
+  let sfRsSHstocked = [];
+  let sfRsSH_NOTstocked = [];
+  let sfRsGLstocked = [];
+  let sfRsGL_NOTstocked = [];
+
+  let stockFilterResults = []; //clear stockFilterResults from previous search
+  // console.log('calcResStockFilterUPC says: stockFilterResults from router.post level===>', stockFilterResults)
+
+  let stockFilterResultsSplitParsedArr = [];
+
+  let stockFilterResultsForCSV = [];
+  let stockFilterResultsForCSVreview = []; //this is for holding data to generate your review excel sheet for Andrea & Brad
+  // console.log('calcResStockFilterUPC says: stockFilterResultsForCSV from router.post level===>', stockFilterResultsForCSV)
+  let csvContainer = [];
+  // console.log('calcResStockFilterUPC says: csvContainer from router.post level===>', csvContainer)
+
+  const postBody = req.body;
+  console.log("calcResStockFilterUPC says: postBody==>", postBody);
+  console.log(
+    "calcResStockFilterUPC says: postBody['fldArrToPostPost']==>",
+    postBody["fldArrToPostPost"]
+  );
+  console.log(
+    "calcResStockFilterUPC says: postBody['fldArrToPostPost'][0]==>",
+    postBody["fldArrToPostPost"][0]
+  );
+
+  let todaysDateRaw = new Date();
+  let todaysDateRaw_iso = todaysDateRaw.toISOString();
+  let todaysDateRaw_split = todaysDateRaw_iso.split("T");
+  let todaysDate = todaysDateRaw_split[0];
+  console.log(`todaysDate==> ${todaysDate}`);
+
+  let oneYearAgoRaw_pre = todaysDateRaw;
+  oneYearAgoRaw_pre.setFullYear(todaysDateRaw.getFullYear() - 1);
+
+  let oneYearAgoRaw_iso = oneYearAgoRaw_pre.toISOString();
+  let oneYearAgoRaw_split = oneYearAgoRaw_iso.split("T");
+  let oneYearAgo = oneYearAgoRaw_split[0];
+  console.log(`oneYearAgo==> ${oneYearAgo}`);
+
+  // let storeNameArr = ['Indiana', 'Saint Matthews', 'Middletown', 'Springhurst', 'Gardiner Lane']
+  let storeNumberArr = ["IN", "SM", "MT", "SPR", "GL"];
+  let storeAbbrevArr = ["IND", "SM", "MT", "SH", "GL"];
+
+  let saniRegex1 = /(\[)|(\])/g;
+  let saniRegex2 = /(?<=},),+(?={)/g; //for some reason, we sometimes get the following format for stockFilterResultsToStringPreSani:
+  // {},,{} so we need to convert that to {},{}
+  let saniRegex3 = /(,$)/g; //for some reason, we sometimes get the following format for stockFilterResultsToStringPreSani:
+  // {}, so we need to convert that to {}; $ = end of string anchor (^ = beginning of string anchor)
+  let saniRegex4 = /(^,)/g; //if a store doesn't carry all items in set, we get the following format for stockFilterResultsToStringPreSani:
+  // ,{} so we need to convert that to {}; $ = end of string anchor (^ = beginning of string anchor)
+
+  /* X(?=Y) 	Positive lookahead 	X if followed by Y
+   * (?<=Y)X 	Positive lookbehind 	X if after Y
+   * ==t0d==>you can combine the 2==> (?<=A)X(?=B) to yield: "X if after A and followed by B" <==t0d==*/
+  let splitRegex1 = /(?<=}),(?={)/g;
+  let splitRegex2 = /(?<=}),,(?={)/g; //for some reason, we sometimes get the following format for stockFilterResultsToStringPreSani:
+  // {},,{}, so we need to convert that to {},{}
+
+  function showStockFilterResults(rows) {
+    let nhcrtRows = rows;
+
+    for (let i = 0; i < nhcrtRows.length; i++) {
+      for (let j = 0; j < storeNumberArr.length; j++) {
+        storeNumber = storeNumberArr[j];
+        storeAbbrev = storeAbbrevArr[j];
+
+        function calcResStockFilterUPC(storeNumber, storeAbbrev) {
+          if (nhcrtRows[i]["stoNumber"] == storeNumber) {
+            let rsltsObj = {};
+            rsltsObj["ri_t0d"] = i;
+            rsltsObj[`${storeAbbrev}_UPCs`] = nhcrtRows[i]["invScanCode"];
+            if (
+              nhcrtRows[i]["invLastreceived"] > oneYearAgo ||
+              nhcrtRows[i]["invLastsold"] > oneYearAgo ||
+              nhcrtRows[i]["invOnhand"] > 0
+            ) {
+              rsltsObj[`${storeAbbrev}stocked`] = nhcrtRows[i]["invScanCode"];
+              if (nhcrtRows[i]["stoNumber"] == "IN") {
+                sfRsINDstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "SM") {
+                sfRsSMstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "MT") {
+                sfRsMTstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "SPR") {
+                sfRsSHstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "GL") {
+                sfRsGLstocked.push(rsltsObj);
+              }
+            } else {
+              rsltsObj[`${storeAbbrev}_NOTstocked`] =
+                nhcrtRows[i]["invScanCode"];
+              if (nhcrtRows[i]["stoNumber"] == "IN") {
+                sfRsIND_NOTstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "SM") {
+                sfRsSM_NOTstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "MT") {
+                sfRsMT_NOTstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "SPR") {
+                sfRsSH_NOTstocked.push(rsltsObj);
+              }
+              if (nhcrtRows[i]["stoNumber"] == "GL") {
+                sfRsGL_NOTstocked.push(rsltsObj);
+              }
+            }
+            // stockFilterResults.push(rsltsObj)
+          }
+        }
+        calcResStockFilterUPC(storeNumber, storeAbbrev);
+      }
+    }
+
+    console.log(`sfRsINDstocked==> ${sfRsINDstocked}`);
+    console.log(
+      `JSON.stringify(sfRsINDstocked)==> ${JSON.stringify(sfRsINDstocked)}`
+    );
+    console.log(
+      `typeof JSON.stringify(sfRsINDstocked)==> ${typeof JSON.stringify(
+        sfRsINDstocked
+      )}`
+    );
+    let JSONstringifysfRsINDstocked = JSON.stringify(sfRsINDstocked);
+    console.log(
+      `typeof JSONstringifysfRsINDstocked==> ${typeof JSONstringifysfRsINDstocked}`
+    );
+
+    sfRsINDstockedSani = JSON.stringify(sfRsINDstocked).replace(saniRegex1, "");
+    sfRsIND_NOTstockedSani = JSON.stringify(sfRsIND_NOTstocked).replace(
+      saniRegex1,
+      ""
+    );
+    sfRsSMstockedSani = JSON.stringify(sfRsSMstocked).replace(saniRegex1, "");
+    sfRsSM_NOTstockedSani = JSON.stringify(sfRsSM_NOTstocked).replace(
+      saniRegex1,
+      ""
+    );
+    sfRsMTstockedSani = JSON.stringify(sfRsMTstocked).replace(saniRegex1, "");
+    sfRsMT_NOTstockedSani = JSON.stringify(sfRsMT_NOTstocked).replace(
+      saniRegex1,
+      ""
+    );
+    sfRsSHstockedSani = JSON.stringify(sfRsSHstocked).replace(saniRegex1, "");
+    sfRsSH_NOTstockedSani = JSON.stringify(sfRsSH_NOTstocked).replace(
+      saniRegex1,
+      ""
+    );
+    sfRsGLstockedSani = JSON.stringify(sfRsGLstocked).replace(saniRegex1, "");
+    sfRsGL_NOTstockedSani = JSON.stringify(sfRsGL_NOTstocked).replace(
+      saniRegex1,
+      ""
+    );
+
+    stockFilterResults.push(
+      sfRsINDstockedSani,
+      sfRsIND_NOTstockedSani,
+      sfRsSMstockedSani,
+      sfRsSM_NOTstockedSani,
+      sfRsMTstockedSani,
+      sfRsMT_NOTstockedSani,
+      sfRsSHstockedSani,
+      sfRsSH_NOTstockedSani,
+      sfRsGLstockedSani,
+      sfRsGL_NOTstockedSani
+    );
+
+    let stockFilterResultsToStringPreSani = stockFilterResults.toString();
+    let stockFilterResultsToString = stockFilterResultsToStringPreSani
+      .replace(saniRegex2, "")
+      .replace(saniRegex3, "")
+      .replace(saniRegex4, "");
+    //for some reason, we sometimes get the following format for stockFilterResultsToStringPreSani:
+    // {},,{}, so we need to convert that to {},{}
+
+    console.log(`stockFilterResultsToString==> ${stockFilterResultsToString}`);
+    let stockFilterResultsSplit = stockFilterResultsToString.split(splitRegex1);
+    console.log(
+      `stockFilterResultsSplit.length==> ${stockFilterResultsSplit.length}`
+    );
+    // console.log(`stockFilterResultsSplit[0]==> ${stockFilterResultsSplit[0]}`)
+    console.log(`stockFilterResultsSplit==> ${stockFilterResultsSplit}`);
+    console.log(
+      `typeof stockFilterResultsSplit[0]==> ${typeof stockFilterResultsSplit[0]}`
+    );
+    console.log(
+      `typeof JSON.parse(stockFilterResultsSplit[0])==> ${typeof JSON.parse(
+        stockFilterResultsSplit[0]
+      )}`
+    );
+
+    for (let k = 0; k < stockFilterResultsSplit.length; k++) {
+      let stockFilterResultsSplitParsed = JSON.parse(
+        stockFilterResultsSplit[k]
+      );
+      stockFilterResultsSplitParsedArr.push(stockFilterResultsSplitParsed);
+    }
+    console.log(
+      `stockFilterResultsSplitParsedArr[0]['ri_t0d']==> ${stockFilterResultsSplitParsedArr[0]["ri_t0d"]}`
+    );
+  }
+
+  function queryNhcrtTable() {
+    connection.query(
+      `SELECT * FROM ${tableName}`,
+      function (err, rows, fields) {
+        if (err) throw err;
+        showStockFilterResults(rows);
+
+        res.json({
+          sfResRows: stockFilterResultsSplit,
+          loadedSqlTbl: loadedSqlTbl,
+          sfRsINDstocked: sfRsINDstocked,
+          sfRsIND_NOTstocked: sfRsIND_NOTstocked,
+          sfRsSMstocked: sfRsSMstocked,
+          sfRsSM_NOTstocked: sfRsSM_NOTstocked,
+          sfRsMTstocked: sfRsMTstocked,
+          sfRsMT_NOTstocked: sfRsMT_NOTstocked,
+          sfRsSHstocked: sfRsSHstocked,
+          sfRsSH_NOTstocked: sfRsSH_NOTstocked,
+          sfRsGLstocked: sfRsGLstocked,
+          sfRsGL_NOTstocked: sfRsGL_NOTstocked,
+          stockFilterResultsSplitParsedArr: stockFilterResultsSplitParsedArr,
+        });
+      }
+    );
+  }
+
+  queryNhcrtTable();
+}
