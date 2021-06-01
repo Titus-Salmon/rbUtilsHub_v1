bcc501fc95e5b56f49ffef7f83cacfd45caafae8
tsalm0n <tsalm0n@twc.com>
2021-05-31 23:18:24 -0400

    nav & favicon

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,46 +1,46 @@
 <script>
-export let segment;
-// import DkMdBtn from "./DkMdBtn.svelte"; //t0d
+  export let segment;
+  // import DkMdBtn from "./DkMdBtn.svelte"; //t0d
 </script>
 
 <style>
-/* nav {
+  /* nav {
   border-bottom: 1px solid rgba(255, 62, 0, 0.1);
   font-weight: 300;
   padding: 0 1em;
 } */
-ul {
-  margin: 0;
-  padding: 0;
-}
-/* clearfix */
-ul::after {
-  content: "";
-  display: block;
-  clear: both;
-}
-li {
-  display: block;
-  float: left;
-}
-[aria-current] {
-  position: relative;
-  display: inline-block;
-}
-[aria-current]::after {
-  position: absolute;
-  content: "";
-  width: calc(100% - 1em);
-  height: 2px;
-  background-color: rgb(255, 62, 0);
-  display: block;
-  bottom: -1px;
-}
-a {
-  text-decoration: none;
-  padding: 1em 0.5em;
-  display: block;
-}
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+  li {
+    display: block;
+    float: left;
+  }
+  [aria-current] {
+    position: relative;
+    display: inline-block;
+  }
+  [aria-current]::after {
+    position: absolute;
+    content: "";
+    width: calc(100% - 1em);
+    height: 2px;
+    background-color: rgb(255, 62, 0);
+    display: block;
+    bottom: -1px;
+  }
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+  }
 </style>
 
 <nav>
@@ -84,8 +84,8 @@ a {
       <div>
         <li>
           <a
-            aria-current="{segment === 'toDo' ? 'page' : undefined}"
-            href="/pages/pg_toDo/toDo">toDo</a>
+            aria-current="{segment === 'stockFilterUPC' ? 'page' : undefined}"
+            href="/pages/pg_MySQL/stockFilterUPC">stockFilterUPC</a>
         </li>
       </div>
       <div>
