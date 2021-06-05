1fcac603124d7223c1a7caf1b63e80f28ed5974b
tsalm0n <tsalm0n@twc.com>
2021-06-05 17:06:45 -0400

    calcResults highlight troubleshooting

--- src/libT0d/calcResults/tableHighlight/resetRows.js
+++ src/libT0d/calcResults/tableHighlight/resetRows.js
@@ -2,7 +2,7 @@ function resetRows() {
   const rsltTblBdy = document.getElementById("rsltTblBdy");
   let tblRows = rsltTblBdy.getElementsByTagName("tr"); //targets all rows in table
 
-  for (i = 0; i < tblRows.length; i++) {
+  for (let i = 0; i < tblRows.length; i++) {
     if (i % 2 == 0) tblRows[i].style.backgroundColor = "var(--surface2)";
   }
 }
