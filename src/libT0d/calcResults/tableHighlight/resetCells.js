ca36bb51eeb4563a12ed2d95595f0e428fa9253a
tsalm0n <tsalm0n@twc.com>
2021-06-05 18:31:35 -0400

    calcResults highlight troubleshooting

--- src/libT0d/calcResults/tableHighlight/resetCells.js
+++ src/libT0d/calcResults/tableHighlight/resetCells.js
@@ -98,7 +98,28 @@ function resetCells(tblData) {
       //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
       // ediCostMod_cell.style.backgroundColor = "black";
       // ediCostMod_cell.style.color = "lime";
-      for (let i = 0; i < ediCostMod_cell_array.length; i++) {
+    }
+
+    if (charm_cell && basePrice_cell) {
+      //only do calcs on these cells if they exist (because sometimes your table results
+      //won't include such cells/columns), otherwise you'll throw an error
+      //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
+      // charm_cell.style.backgroundColor = "black";
+      // charm_cell.style.color = "lime";
+    }
+  }
+
+  console.log(
+    `ediCostMod_cell_array.length==> ${ediCostMod_cell_array.length}`
+  );
+  console.log(`charm_cell_array.length==> ${charm_cell_array.length}`);
+
+  for (let i = 0; i < ediCostMod_cell_array.length; i++) {
+    for (let k = 0; k < tblCells.length; k++) {
+      for (let m = 0; m < tblCols.length; m++) {
+        if (tblCols[m] === "ediCostMod") {
+          ediCostMod_cell = tblCells[k].parentNode.childNodes[m];
+        }
         if (i % 2 == 0) {
           ediCostMod_cell.style.backgroundColor = "black";
           ediCostMod_cell.style.color = "lime";
@@ -108,15 +129,14 @@ function resetCells(tblData) {
         }
       }
     }
+  }
 
-    if (charm_cell && basePrice_cell) {
-      //only do calcs on these cells if they exist (because sometimes your table results
-      //won't include such cells/columns), otherwise you'll throw an error
-
-      //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
-      // charm_cell.style.backgroundColor = "black";
-      // charm_cell.style.color = "lime";
-      for (let i = 0; i < charm_cell_array.length; i++) {
+  for (let i = 0; i < charm_cell_array.length; i++) {
+    for (let k = 0; k < tblCells.length; k++) {
+      for (let m = 0; m < tblCols.length; m++) {
+        if (tblCols[m] === "charm") {
+          charm_cell = tblCells[k].parentNode.childNodes[m];
+        }
         if (i % 2 == 0) {
           charm_cell.style.backgroundColor = "black";
           charm_cell.style.color = "lime";
@@ -127,11 +147,6 @@ function resetCells(tblData) {
       }
     }
   }
-
-  console.log(
-    `ediCostMod_cell_array.length==> ${ediCostMod_cell_array.length}`
-  );
-  console.log(`charm_cell_array.length==> ${charm_cell_array.length}`);
 }
 
 export { resetCells };
