d0be5047c89c5c598a08c44e10d8f969959fc47c
tsalm0n <tsalm0n@twc.com>
2021-06-05 22:51:12 -0400

    calcResults highlight troubleshooting

--- src/libT0d/calcResults/tableHighlight/resetCells.js
+++ src/libT0d/calcResults/tableHighlight/resetCells.js
@@ -29,6 +29,9 @@ function resetCells(tblData) {
     }
   }
 
+  console.log(`ediCostModColOffset==> ${ediCostModColOffset}`);
+  console.log(`charmColOffset==> ${charmColOffset}`);
+
   for (let k = 0; k < tblCells.length; k++) {
     for (let m = 0; m < tblCols.length; m++) {
       if (tblCols[m] === "lastCost") {
@@ -36,44 +39,43 @@ function resetCells(tblData) {
       }
       if (tblCols[m] === "ediCostMod") {
         ediCostMod_cell = tblCells[k].parentNode.childNodes[m];
+        let rowNum = (k - ediCostModColOffset) / ediCostModColNum;
+        if (rowNum % 2 == 0) {
+          //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
+          ediCostMod_cell.style.backgroundColor = "black";
+          ediCostMod_cell.style.color = "lime";
+        } else {
+          //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
+          ediCostMod_cell.style.backgroundColor = "var(--surface2)";
+          ediCostMod_cell.style.color = "lime";
+        }
       }
       if (tblCols[m] === "sib_baseprice") {
         basePrice_cell = tblCells[k].parentNode.childNodes[m];
       }
       if (tblCols[m] === "charm") {
         charm_cell = tblCells[k].parentNode.childNodes[m];
+        let rowNum = (k - charmColOffset) / charmColNum;
+        if (rowNum % 2 == 0) {
+          //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
+          charm_cell.style.backgroundColor = "black";
+          charm_cell.style.color = "lime";
+        } else {
+          //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
+          charm_cell.style.backgroundColor = "var(--surface2)";
+          charm_cell.style.color = "lime";
+        }
       }
     }
-    if (ediCostMod_cell && lastCost_cell) {
-      //only do calcs on these cells if they exist (because sometimes your table results
-      //won't include such cells/columns), otherwise you'll throw an error
+    // if (ediCostMod_cell && lastCost_cell) {
+    //   //only do calcs on these cells if they exist (because sometimes your table results
+    //   //won't include such cells/columns), otherwise you'll throw an error
+    // }
 
-      let rowNum = (k - ediCostModColOffset) / ediCostModColNum;
-      if (rowNum % 2 == 0) {
-        //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
-        ediCostMod_cell.style.backgroundColor = "black";
-        ediCostMod_cell.style.color = "lime";
-      } else {
-        //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
-        ediCostMod_cell.style.backgroundColor = "var(--surface2)";
-        ediCostMod_cell.style.color = "lime";
-      }
-    }
-
-    if (charm_cell && basePrice_cell) {
-      //only do calcs on these cells if they exist (because sometimes your table results
-      //won't include such cells/columns), otherwise you'll throw an error
-      let rowNum = (k - charmColOffset) / charmColNum;
-      if (rowNum % 2 == 0) {
-        //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
-        charm_cell.style.backgroundColor = "black";
-        charm_cell.style.color = "lime";
-      } else {
-        //clear all highlighting as first step, in order that subsequent paginations don't retain highlighting from previous page
-        charm_cell.style.backgroundColor = "var(--surface2)";
-        charm_cell.style.color = "lime";
-      }
-    }
+    // if (charm_cell && basePrice_cell) {
+    //   //only do calcs on these cells if they exist (because sometimes your table results
+    //   //won't include such cells/columns), otherwise you'll throw an error
+    // }
   }
 }
 
