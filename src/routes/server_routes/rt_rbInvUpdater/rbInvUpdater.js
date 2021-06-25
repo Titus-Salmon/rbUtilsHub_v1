5fa7b515b9bbe68f7ec567612a69e077a8ad0600
tsalm0n <tsalm0n@twc.com>
2021-06-25 03:56:50 -0400

    optimize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/rbInvUpdater.js
+++ src/routes/server_routes/rt_rbInvUpdater/rbInvUpdater.js
@@ -685,19 +685,8 @@ export async function post(req, res, next) {
         rbInvJoinObj_ind["new_inv_nameIND"] = indRows[i]["new_inv_name"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_ind["new_inv_in_stockIND"] =
           indRows[i]["new_inv_in_stock"];
-        // if (indRows[i]['new_inv_in_stock'] = null) {
-        //   rbInvJoinObj_ind['new_inv_in_stockIND'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_ind['new_inv_in_stockIND'] = indRows[i]['new_inv_in_stock']
-        // }
         rbInvJoinObj_ind["old_inv_in_stockIND"] =
           indRows[i]["old_inv_in_stock"];
-        // if (indRows[i]['old_inv_in_stock'] = null) {
-        //   rbInvJoinObj_ind['old_inv_in_stockIND'] == 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_ind['old_inv_in_stockIND'] = indRows[i]['old_inv_in_stock']
-        // }
-
         rbInvJoinArr_ind.push(rbInvJoinObj_ind);
       }
 
@@ -707,18 +696,7 @@ export async function post(req, res, next) {
         rbInvJoinObj_sm["new_inv_upcSM"] = smRows[i]["new_inv_upc"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sm["new_inv_nameSM"] = smRows[i]["new_inv_name"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sm["new_inv_sm_stockSM"] = smRows[i]["new_inv_sm_stock"];
-        // if (smRows[i]['new_inv_sm_stock'] = null) {
-        //   rbInvJoinObj_sm['new_inv_sm_stockSM'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_sm['new_inv_sm_stockSM'] = smRows[i]['new_inv_sm_stock']
-        // }
         rbInvJoinObj_sm["old_inv_sm_stockSM"] = smRows[i]["old_inv_sm_stock"];
-        // if (smRows[i]['old_inv_sm_stock'] = null) {
-        //   rbInvJoinObj_sm['old_inv_sm_stockSM'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_sm['old_inv_sm_stockSM'] = smRows[i]['old_inv_sm_stock']
-        // }
-
         rbInvJoinArr_sm.push(rbInvJoinObj_sm);
       }
 
@@ -728,18 +706,7 @@ export async function post(req, res, next) {
         rbInvJoinObj_mt["new_inv_upcMT"] = mtRows[i]["new_inv_upc"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_mt["new_inv_nameMT"] = mtRows[i]["new_inv_name"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_mt["new_inv_mt_stockMT"] = mtRows[i]["new_inv_mt_stock"];
-        // if (mtRows[i]['new_inv_mt_stock'] = null) {
-        //   rbInvJoinObj_mt['new_inv_mt_stockMT'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_mt['new_inv_mt_stockMT'] = mtRows[i]['new_inv_mt_stock']
-        // }
         rbInvJoinObj_mt["old_inv_mt_stockMT"] = mtRows[i]["old_inv_mt_stock"];
-        // if (mtRows[i]['old_inv_mt_stock'] = null) {
-        //   rbInvJoinObj_mt['old_inv_mt_stockMT'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_mt['old_inv_mt_stockMT'] = mtRows[i]['old_inv_mt_stock']
-        // }
-
         rbInvJoinArr_mt.push(rbInvJoinObj_mt);
       }
 
@@ -749,18 +716,7 @@ export async function post(req, res, next) {
         rbInvJoinObj_sh["new_inv_upcSH"] = shRows[i]["new_inv_upc"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sh["new_inv_nameSH"] = shRows[i]["new_inv_name"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sh["new_inv_sh_stockSH"] = shRows[i]["new_inv_sh_stock"];
-        // if (shRows[i]['new_inv_sh_stock'] = null) {
-        //   rbInvJoinObj_sh['new_inv_sh_stockSH'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_sh['new_inv_sh_stockSH'] = shRows[i]['new_inv_sh_stock']
-        // }
         rbInvJoinObj_sh["old_inv_sh_stockSH"] = shRows[i]["old_inv_sh_stock"];
-        // if (shRows[i]['old_inv_sh_stock'] = null) {
-        //   rbInvJoinObj_sh['old_inv_sh_stockSH'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_sh['old_inv_sh_stockSH'] = shRows[i]['old_inv_sh_stock']
-        // }
-
         rbInvJoinArr_sh.push(rbInvJoinObj_sh);
       }
 
@@ -770,18 +726,7 @@ export async function post(req, res, next) {
         rbInvJoinObj_gl["new_inv_upcGL"] = glRows[i]["new_inv_upc"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_gl["new_inv_nameGL"] = glRows[i]["new_inv_name"]; //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_gl["new_inv_gl_stockGL"] = glRows[i]["new_inv_gl_stock"];
-        // if (glRows[i]['new_inv_gl_stock'] = null) {
-        //   rbInvJoinObj_gl['new_inv_gl_stockGL'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_gl['new_inv_gl_stockGL'] = glRows[i]['new_inv_gl_stock']
-        // }
         rbInvJoinObj_gl["old_inv_gl_stockGL"] = glRows[i]["old_inv_gl_stock"];
-        // if (glRows[i]['old_inv_gl_stock'] = null) {
-        //   rbInvJoinObj_gl['old_inv_gl_stockGL'] = 'EMPTY'
-        // } else {
-        //   rbInvJoinObj_gl['old_inv_gl_stockGL'] = glRows[i]['old_inv_gl_stock']
-        // }
-
         rbInvJoinArr_gl.push(rbInvJoinObj_gl);
       }
 
@@ -808,7 +753,7 @@ export async function post(req, res, next) {
         rbInvJoinArr_shSani,
         rbInvJoinArr_glSani
       );
-
+      console.log(`searchResults==> ${searchResults}`);
       let searchResultsToString = searchResults.toString();
       searchResultsSplit = searchResultsToString.split(splitRegex1);
       console.log(`searchResultsSplit.length==> ${searchResultsSplit.length}`);
