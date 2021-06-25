6c680641f033770de6c93a53c69944b961bbc702
tsalm0n <tsalm0n@twc.com>
2021-06-25 00:30:04 -0400

    optimize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/rbInvUpdater.js
+++ src/routes/server_routes/rt_rbInvUpdater/rbInvUpdater.js
@@ -95,54 +95,56 @@ export async function post(req, res, next) {
             console.error(error);
           }
 
-          async function csvGenerator() {
-            //begin csv generator //////////////////////////////////////////////////////////////////////////
-            const { Parser } = require("json2csv");
-
-            const fields = [
-              "ri_t0d",
-              "INV_ScanCode",
-              "sto_number",
-              "inv_lastreceived",
-              "inv_lastsold",
-              "inv_onhand",
-              "inv_onorder",
-              "inv_intransit",
-            ];
-
-            const opts = {
-              fields,
-            };
-
-            try {
-              console.log(
-                "catapultResArr[0] from json2csv======>>",
-                catapultResArr[0]
-              );
-              const parser = new Parser(opts);
-              const csv = parser.parse(catapultResArr);
-              console.log("csv.length=====>>", csv.length);
-              fs.writeFile(
-                `${process.cwd()}/static/csv/rb_inv_nhcrt.csv`,
-                csv,
-                function (err) {
-                  if (err) throw err;
-                  console.log("~~~~~>>rb_inv_nhcrt.csvsaved<<~~~~~");
-                }
-              );
-            } catch (err) {
-              console.error(err);
-            }
-            //end csv generator //////////////////////////////////////////////////////////////////////////
-          }
-          catapultResults(result).then(csvGenerator());
+          catapultResults(result);
           console.log(
             `result.length from catapultResults(result)~~~> ${result.length}`
           );
         });
       });
     }
-    await odbcPart(catapultDbQuery).then(createNhcrtRbInvTable());
+    async function csvGenerator() {
+      //begin csv generator //////////////////////////////////////////////////////////////////////////
+      const { Parser } = require("json2csv");
+
+      const fields = [
+        "ri_t0d",
+        "INV_ScanCode",
+        "sto_number",
+        "inv_lastreceived",
+        "inv_lastsold",
+        "inv_onhand",
+        "inv_onorder",
+        "inv_intransit",
+      ];
+
+      const opts = {
+        fields,
+      };
+
+      try {
+        console.log(
+          "catapultResArr[0] from json2csv======>>",
+          catapultResArr[0]
+        );
+        const parser = new Parser(opts);
+        const csv = parser.parse(catapultResArr);
+        console.log("csv.length=====>>", csv.length);
+        fs.writeFile(
+          `${process.cwd()}/static/csv/rb_inv_nhcrt.csv`,
+          csv,
+          function (err) {
+            if (err) throw err;
+            console.log("~~~~~>>rb_inv_nhcrt.csvsaved<<~~~~~");
+          }
+        );
+      } catch (err) {
+        console.error(err);
+      }
+      //end csv generator //////////////////////////////////////////////////////////////////////////
+    }
+    await odbcPart(catapultDbQuery)
+      .then(csvGenerator())
+      .then(createNhcrtRbInvTable());
   }
 
   async function createNhcrtRbInvTable() {
