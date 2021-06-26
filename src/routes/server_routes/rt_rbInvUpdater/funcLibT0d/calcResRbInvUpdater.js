4f6a5e7cd8b1bc885f2f58d7ee70d7f70d7f30ec
tsalm0n <tsalm0n@twc.com>
2021-06-26 02:09:21 -0400

    modularize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/calcResRbInvUpdater.js
+++ src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/calcResRbInvUpdater.js
@@ -1,5 +1,15 @@
 import { rbInvAudit } from "../funcLibT0d/rbInvAudit";
 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
 async function calcResRbInvUpdater() {
   let wishlistIgnore = [];
   let wishlistUpdate = [];
