eaa39d1c0f592a8397f1a001292a8258d6e26f1e
tsalm0n <tsalm0n@twc.com>
2021-06-26 01:58:19 -0400

    modularize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/createNhcrtRbInvTable.js
+++ src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/createNhcrtRbInvTable.js
@@ -1,5 +1,15 @@
 import { populateNhcrtRbInvTable } from "../funcLibT0d/populateNhcrtRbInvTable";
 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
 async function createNhcrtRbInvTable() {
   let createNhcrtRbInvTableQuery = `
   DROP TABLE IF EXISTS nhcrtRbInv;
