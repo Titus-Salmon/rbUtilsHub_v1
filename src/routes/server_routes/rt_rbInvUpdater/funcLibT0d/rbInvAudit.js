175e4d5b596ee98b942bb476689052442d43579c
tsalm0n <tsalm0n@twc.com>
2021-06-26 02:23:18 -0400

    modularize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/rbInvAudit.js
+++ src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/rbInvAudit.js
@@ -1,3 +1,13 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
 async function rbInvAudit() {
   let rbInvJoinArr_ind = [];
   let rbInvJoinArr_sm = [];
