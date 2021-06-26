39fd97bd20c35c21d936d276695399cc75fb470a
tsalm0n <tsalm0n@twc.com>
2021-06-25 23:27:03 -0400

    modularize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/rb_inventory_query.js
+++ src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/rb_inventory_query.js
@@ -1,3 +1,13 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
 async function rb_inventory_query() {
   connection
     .query(rb_inventoryQuery, function (err, rows, fields) {
