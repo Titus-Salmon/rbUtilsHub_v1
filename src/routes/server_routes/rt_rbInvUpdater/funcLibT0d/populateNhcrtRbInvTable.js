711d4b30659e822498e4d0e03e7dc0476df9507d
tsalm0n <tsalm0n@twc.com>
2021-06-26 02:06:31 -0400

    modularize rbInvUpdater

--- src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/populateNhcrtRbInvTable.js
+++ src/routes/server_routes/rt_rbInvUpdater/funcLibT0d/populateNhcrtRbInvTable.js
@@ -1,5 +1,15 @@
 import { calcResRbInvUpdater } from "../funcLibT0d/calcResRbInvUpdater";
 
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true, //MUST HAVE to make more than 1 sql statement in a single query
+});
+
 async function populateNhcrtRbInvTable(populateNhcrtRbInvTableQuery) {
   connection
     .query(populateNhcrtRbInvTableQuery, function (error, response) {
