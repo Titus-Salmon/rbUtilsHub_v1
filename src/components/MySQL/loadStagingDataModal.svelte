4b6e9d3c48dce917eef6abd1157031d077d1b123
tsalm0n <tsalm0n@twc.com>
2021-07-13 01:00:23 -0400

    add poRtlUpdater module

--- src/components/MySQL/loadStagingDataModal.svelte
+++ src/components/MySQL/loadStagingDataModal.svelte
@@ -58,10 +58,8 @@
     <div style="text-align:center">
       <label for="tableType">Table type</label>
       <select name="tableType" id="tableType" bind:value="{tableType}">
-        <option value="edi">EDI</option>
         <option value="pprv_edi">pprv_edi</option>
         <option value="pprv">pprv</option>
-        <option value="other">Other</option>
       </select>
     </div>
   </div>
