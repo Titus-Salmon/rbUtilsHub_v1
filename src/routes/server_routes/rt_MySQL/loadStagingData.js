5789ca76eef74616117ecca2ae3d299731c505db
tsalm0n <tsalm0n@twc.com>
2021-07-13 00:18:37 -0400

    add poRtlUpdater module

--- src/routes/server_routes/rt_MySQL/loadStagingData.js
+++ src/routes/server_routes/rt_MySQL/loadStagingData.js
@@ -41,9 +41,9 @@ export async function post(req, res, next) {
   console.log(`tableType==> ${tableType}`);
 
   //v//here we are doing some regex & js "split" magic to extract the "catalog" name from the nej table name we're loading (nejTableNameYYYMMDD):
-  let regex1 = /(\d+)/g;
-  let vendorNameSplit1 = tableNameToLoad.split(regex1);
-  let vendorName = vendorNameSplit1[0];
+  // let regex1 = /(\d+)/g;
+  // let vendorNameSplit1 = tableNameToLoad.split(regex1);
+  // let vendorName = vendorNameSplit1[0];
 
   if (tableType.toLowerCase() === "edi") {
     let regex1 = /(\d+)/g;
@@ -72,6 +72,12 @@ export async function post(req, res, next) {
     let vendorNameSplit3 = vendorNameSplit2.split(regex1);
     let vendorName = vendorNameSplit3[0];
     console.log(`vendorName==> ${vendorName}`);
+  }
+
+  if (tableType.toLowerCase() === "other") {
+    let regex1 = /(\d+)/g;
+    let vendorNameSplit1 = tableNameToLoad.split(regex1);
+    let vendorName = vendorNameSplit1[0];
   }
   //^//here we are doing some regex & js "split" magic to extract the "catalog" name from the nej table name we're loading (nejTableNameYYYMMDD):
 
