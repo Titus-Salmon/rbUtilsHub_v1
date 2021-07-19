2aa523082404c8a62977dbed306501fa34bf3515
tsalm0n <tsalm0n@twc.com>
2021-07-18 22:04:03 -0400

    add poRtlUpdater module

---
+++ src/components/TSQL/queryModals/poQueryModal1.svelte
@@ -0,0 +1,38 @@
+<script>
+  let catapultQueryText;
+  let queryText = `
+select
+si.inv_pk, si.inv_cpk, si.inv_scancode,
+pow.POW_PK, pow.POW_ReceiveDate, pow.POW_InvoiceNumber,
+pod.POD_POW_FK, pod.POD_INV_FK, pod.POD_INV_CFK, pod.POD_OrderQuantity, pod.POD_ReceiveQuantity, pod.POD_ReceiveCost, pod.POD_ORDQuantity,
+pod.POD_ASCQuantity, pod.POD_Received, pod.POD_BackOrder, pod.POD_CommitCost, pod.POD_CommitRetail, pod.POD_Timestamp, pod.POD_PkgCost,
+pod.POD_OriginalReceiveCost, pod.POD_TotalCommittedCost, pod.POD_OriginalTotalCommittedCost, pod.POD_PreFreightTotalCommittedCost,
+pod.POD_PreDiscountTotalCommittedCost, pod.POD_ApplyDiscount, pod.POD_TotalCost, pod.POD_CostUpdateSource, pod.POD_ShipQuantity, pod.POD_Verified,
+pod.POD_PkgCharges, pod.POD_UnitCharges, pod.POD_TotalCharges, pod.POD_PkgOrderCost, pod.POD_UnitOrderCost, pod.POD_TotalOrderCost,
+pod.POD_CommitOnHand, pod.POD_CommitDivider,
+vim.inv_scancode, vim.ord_supplierstocknumber, vim.inv_receiptalias, vim.asc_scancode, vim.asc_receiptalias, vim.asc_quantity, vim.inv_lastcost, 
+vim.sib_baseprice, vim.ven_companyname, vim.dpt_name, vim.dpt_number, vim.oup_name, vim.brd_name, vim.inv_size, vim.sto_number, 
+vim.inv_discontinued, vim.pi1_description, vim.pi2_description
+from catapult.ecrs.PurchaseOrderWorksheetData pod
+left join catapult.ecrs.PurchaseOrderWorksheet pow on trim(pow.pow_pk) = trim(pod.pod_pow_fk)
+left join catapult.ecrs.StockInventory si on trim(si.inv_pk) = trim(pod.pod_inv_fk) and trim(si.inv_cpk) = trim(pod.pod_inv_cfk)
+left join catapult.ecrs.v_inventorymaster vim on trim(vim.inv_scancode) = trim(si.inv_scancode)
+where trim(pod.POD_TimeStamp) > '2021-06-20'
+and trim(pow.POW_ReceiveDate) > '2021-06-20'
+and trim(vim.ven_companyname) = 'EDI-KEHE'
+and trim(pod.pod_received) = '1'
+order by pow.POW_ReceiveDate, pow.POW_InvoiceNumber, vim.ven_companyname, vim.inv_scancode, vim.pi1_description, vim.pi2_description`;
+</script>
+
+<div style="text-align:center">
+  <textarea
+    class="query"
+    id="catapultQueryText"
+    name="tblQryPost"
+    cols="160"
+    rows="18"
+    wrap="soft"
+    bind:this="{catapultQueryText}">
+    {queryText}
+  </textarea>
+</div>
--- src/components/TSQL/querySelectDropdown.svelte
+++ src/components/TSQL/querySelectDropdown.svelte
@@ -1,5 +1,7 @@
 <script>
   import VimQueryModal from "./queryModals/vimQueryModal.svelte";
+  import PoQueryModal1 from "./queryModals/poQueryModal1.svelte";
+  import PoAuditQueryModal1 from "./queryModals/poAuditQueryModal1.svelte";
 
   let selected;
 </script>
@@ -10,9 +12,17 @@
   <label for="querySelect">Select Query:</label>
   <select name="querySelect" id="querySelect" bind:value="{selected}">
     <option value="vimQuery1">vimQuery1</option>
+    <option value="poQuery1">poQuery1</option>
+    <option value="poAuditQuery1">poAuditQuery1</option>
   </select>
 </div>
 
 {#if selected === "vimQuery1"}
   <VimQueryModal />
+{/if}
+{#if selected === "poQuery1"}
+  <PoQueryModal1 />
+{/if}
+{#if selected === "poAuditQuery1"}
+  <PoAuditQueryModal1 />
 {/if}
